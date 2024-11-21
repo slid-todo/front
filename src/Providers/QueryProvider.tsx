c2518ce10ec67fc9c44356446380cd9e8a9cc98f
beom <gb3187@naver.com>
2024-11-21 13:45:55 +0900

    Setting: 프로젝트 초기 세팅 (pxToRem, tanstack-query)

---
+++ src/Providers/QueryProvider.tsx
@@ -0,0 +1,23 @@
+"use client";
+
+import React from "react";
+import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
+import { ReactQueryDevtools } from "@tanstack/react-query-devtools";
+
+// QueryClient 인스턴스 생성
+const queryClient = new QueryClient();
+
+interface QueryProviderProps {
+  children: React.ReactNode;
+}
+
+const QueryProvider: React.FC<QueryProviderProps> = ({ children }) => {
+  return (
+    <QueryClientProvider client={queryClient}>
+      {children}
+      <ReactQueryDevtools initialIsOpen={true} />
+    </QueryClientProvider>
+  );
+};
+
+export default QueryProvider;
---
+++ src/app/page.tsx
@@ -0,0 +1,7 @@
+export default function Home() {
+  return (
+    <div>
+      <button className="w-100 h-100">안녕</button>
+    </div>
+  );
+}
