0dbc0bf8927855676ba2a5279cccb92405919052
beom <gb3187@naver.com>
2024-11-27 15:56:48 +0900

    Feat: react-icons 설치 및 폰트 및 색상 변경

--- src/components/common/Modal/TodoModal.tsx
+++ src/components/common/Modal/TodoModal.tsx
@@ -1,3 +1,6 @@
+import { ImCancelCircle } from 'react-icons/im';
+import { FaPlus } from 'react-icons/fa';
+
 const TodoModal = () => {
   return (
     <div className="fixed inset-0 z-50 flex items-center justify-center bg-black/80">
@@ -6,15 +9,13 @@ const TodoModal = () => {
           <div className="flex flex-col gap-24 self-stretch">
             {/* 헤더 */}
             <div className="flex items-center justify-between">
-              <span className="flex text-18 font-bold leading-7 text-slate-800">
-                할 일 생성
-              </span>
-              <p>X아이콘</p>
+              <span className="text-lg-bold text-slate-800">할 일 생성</span>
+              <ImCancelCircle size={24} className="cursor-pointer" />
             </div>
             {/* 헤더 */}
             {/* 제목 */}
             <div className="flex flex-col items-start gap-12 self-stretch">
-              <span>제목</span>
+              <span className="text-base-semibold text-slate-800">제목</span>
               <input
                 className="flex items-center gap-10 self-stretch rounded-12 bg-slate-50 px-24 py-12"
                 placeholder="할 일의 제목을 적어주세요"
@@ -25,25 +26,32 @@ const TodoModal = () => {
             <div className="flex flex-col items-start gap-12">
               {/* 자료 */}
               <div className="flex flex-col items-start gap-12">
-                <span>자료</span>
+                <span className="text-base-semibold text-slate-800">자료</span>
                 <div className="flex items-start gap-12">
-                  <button>파일업로드</button>
-                  <button>링크 첨부</button>
+                  <button className="text-base-medium text-slate-800">
+                    파일업로드
+                  </button>
+                  <button className="text-base-medium text-slate-800">
+                    링크 첨부
+                  </button>
                 </div>
               </div>
               {/* 자료 */}
               {/* 파일업로드 */}
               <div className="flex h-184 w-472 shrink-0 items-center justify-center rounded-12 bg-slate-50">
-                <button className="inline-flex flex-col items-center justify-center gap-2">
-                  <span className="text-4xl">+</span> {/* 크기 조정 가능 */}
-                  <span>파일을 업로드해주세요</span>
+                <button className="inline-flex flex-col items-center justify-center gap-8">
+                  <FaPlus size={24} className="text-slate-400" />
+                  {/* 크기 조정 가능 */}
+                  <span className="text-base-normal text-slate-400">
+                    파일을 업로드해주세요
+                  </span>
                 </button>
               </div>
               {/* 파일업로드 */}
             </div>
             {/* 목표 */}
             <div className="flex flex-col items-start gap-12 self-stretch">
-              <span>목표</span>
+              <span className="text-base-semibold text-slate-800">목표</span>
               <input
                 className="flex flex-col items-start gap-10 self-stretch rounded-12 bg-slate-50 px-20 py-12"
                 placeholder="목표를 선택해주세요"
@@ -53,7 +61,7 @@ const TodoModal = () => {
           </div>
           {/* 확인 버튼 */}
           <button className="flex w-full items-center justify-center gap-10 self-stretch rounded-12 bg-slate-400 py-12">
-            확인
+            <span className="text-base-semibold text-white">확인</span>
           </button>
           {/* 확인 버튼 */}
         </div>
