93f4872378a015ef8091a7450aeabe36d91e5175
beom <gb3187@naver.com>
2024-11-27 18:07:05 +0900

    Feat: 드롭다운 버튼 추가

--- src/components/common/Modal/TodoModal/TodoModalTarget.tsx
+++ src/components/common/Modal/TodoModal/TodoModalTarget.tsx
@@ -1,11 +1,13 @@
+import { FaSortDown } from 'react-icons/fa';
+
 const TodoModalTarget = () => {
   return (
     <div className="flex flex-col items-start gap-12 self-stretch">
       <span className="text-base-semibold text-slate-800">목표</span>
-      <input
-        className="flex flex-col items-start gap-10 self-stretch rounded-12 bg-slate-50 px-20 py-12 text-sm-normal sm:text-base-normal"
-        placeholder="목표를 선택해주세요"
-      />
+      <div className="flex items-center justify-between self-stretch rounded-12 bg-slate-50 px-20 py-12 text-sm-normal sm:text-base-normal">
+        <input className="bg-slate-50" placeholder="목표를 선택해주세요" />
+        <FaSortDown className=" cursor-pointer" size={24} />
+      </div>
     </div>
   );
 };
