7edf938d86f0f0322ccd5e34950e955f087ab4a5
dudwns0213 <dudwns0213@gmail.com>
2024-11-27 14:17:27 +0900

    Test: tailwind color test

--- src/app/(route)/page.tsx
+++ src/app/(route)/page.tsx
@@ -11,6 +11,20 @@ export default function Home() {
       <div className="text-3xl-medium">3xl medium</div>
       <div className="text-3xl-normal">3xl normal</div>
       <div className="text-3xl-light">3xl light</div>
+
+      <div className="text-3xl-bold text-blue-50">3xl bold</div>
+      <div className="text-3xl-semibold text-blue-100">3xl semibold</div>
+      <div className="text-3xl-medium text-blue-200">3xl medium</div>
+      <div className="text-3xl-normal text-blue-300">3xl normal</div>
+      <div className="text-3xl-light text-blue-400">3xl light</div>
+
+      <div className="text-3xl-bold text-blue-500">3xl bold</div>
+      <div className="text-3xl-semibold text-blue-600">3xl semibold</div>
+      <div className="text-3xl-medium text-blue-700">3xl medium</div>
+      <div className="text-3xl-normal text-blue-800">3xl normal</div>
+      <div className="text-3xl-light text-blue-900">3xl light</div>
+
+      <div className="text-3xl-bold text-blue-950">3xl bold</div>
     </div>
   );
 }
