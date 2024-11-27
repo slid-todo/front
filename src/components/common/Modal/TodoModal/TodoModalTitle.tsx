988bd2b75ec7765dfb14fa5d93e8b21c8e1280a1
beom <gb3187@naver.com>
2024-11-27 17:23:42 +0900

    Design: X 아이콘 변경 및 폰트 변경

--- src/components/common/Modal/TodoModal/TodoModalTitle.tsx
+++ src/components/common/Modal/TodoModal/TodoModalTitle.tsx
@@ -1,11 +1,9 @@
 const TodoModalTitle = () => {
   return (
     <div className="flex flex-col items-start gap-12 self-stretch">
-      <span className="text-sm-semibold text-slate-800 sm:text-base-semibold">
-        제목
-      </span>
+      <span className=" text-base-semibold text-slate-800">제목</span>
       <input
-        className="flex items-center gap-10 self-stretch rounded-12 bg-slate-50 px-24 py-12"
+        className="flex items-center gap-10 self-stretch rounded-12 bg-slate-50 px-24 py-12 text-sm-normal sm:text-base-normal"
         placeholder="할 일의 제목을 적어주세요"
       />
     </div>
