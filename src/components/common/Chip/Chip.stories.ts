b7a2dcaa51620a9785d1df3669c3cad77b5e2a43
ejinn1 <ejinn1@naver.com>
2024-11-27 17:19:31 +0900

    Feat: create chip stories

---
+++ src/components/common/Chip/Chip.stories.ts
@@ -0,0 +1,45 @@
+import { Meta, StoryObj } from '@storybook/react';
+import Chip from './Chip';
+
+const meta: Meta<typeof Chip> = {
+  title: 'Components/Chip',
+  component: Chip,
+  parameters: {
+    layout: 'centered',
+  },
+  tags: ['autodocs'],
+  argTypes: {
+    variant: {
+      options: ['default', 'active'],
+    },
+    size: {
+      options: ['sm', 'lg'],
+    },
+  },
+};
+
+export default meta;
+
+export const Default: StoryObj<typeof Chip> = {
+  args: {
+    variant: 'default',
+    size: 'sm',
+    children: 'Default Chip',
+  },
+};
+
+export const Active: StoryObj<typeof Chip> = {
+  args: {
+    variant: 'active',
+    size: 'sm',
+    children: 'Active Chip',
+  },
+};
+
+export const Large: StoryObj<typeof Chip> = {
+  args: {
+    variant: 'default',
+    size: 'lg',
+    children: 'Large Chip',
+  },
+};
