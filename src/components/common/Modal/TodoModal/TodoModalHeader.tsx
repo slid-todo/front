f8921963f767696260b8732c04ce37ca0be8ff9c
beom <gb3187@naver.com>
2024-11-27 17:23:42 +0900

    Design: X 아이콘 변경 및 폰트 변경

--- src/components/common/Modal/TodoModal/TodoModalHeader.tsx
+++ src/components/common/Modal/TodoModal/TodoModalHeader.tsx
@@ -1,9 +1,9 @@
-import { ImCancelCircle } from 'react-icons/im';
+import { IoMdClose } from 'react-icons/io';
 const TodoModalHeader = () => {
   return (
     <div className="flex items-center justify-between">
       <span className="text-lg-bold text-slate-800">할 일 생성</span>
-      <ImCancelCircle size={24} className="cursor-pointer" />
+      <IoMdClose size={24} className="cursor-pointer" />
     </div>
   );
 };
