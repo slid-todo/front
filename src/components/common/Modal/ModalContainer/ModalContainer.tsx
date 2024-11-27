edfa4febb4450d4429f396eea0d5d4397885dbe9
beom <gb3187@naver.com>
2024-11-27 18:40:37 +0900

    Feat: 모달 외부 클릭, X 클릭 시 닫힘 기능 구현

--- src/components/common/Modal/ModalContainer/ModalContainer.tsx
+++ src/components/common/Modal/ModalContainer/ModalContainer.tsx
@@ -1,12 +1,24 @@
+'use client';
+
 import { ReactNode } from 'react';
 
 interface ModalContainerProps {
   children: ReactNode;
+  onClose: () => void;
 }
 
-const ModalContainer = ({ children }: ModalContainerProps) => {
+const ModalContainer = ({ children, onClose }: ModalContainerProps) => {
+  const handleBackgroundClick = (e: React.MouseEvent<HTMLDivElement>) => {
+    // 배경 클릭 시 onClose 호출
+    if (e.target === e.currentTarget) {
+      onClose();
+    }
+  };
   return (
-    <div className="fixed inset-0 z-50 flex items-center justify-center bg-black/80">
+    <div
+      className="fixed inset-0 z-50 flex items-center justify-center bg-black/80"
+      onClick={handleBackgroundClick}
+    >
       {children}
     </div>
   );
