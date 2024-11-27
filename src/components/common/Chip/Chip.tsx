d3d1f0a51a9db07c9bf253b3a62d145456abf02f
ejinn1 <ejinn1@naver.com>
2024-11-27 17:19:31 +0900

    Feat: create chip stories

--- src/components/common/Chip/Chip.tsx
+++ src/components/common/Chip/Chip.tsx
@@ -21,6 +21,31 @@ export interface ChipProps
   size?: keyof typeof chipStyles.size;
 }
 
+/**
+ * Chip 컴포넌트
+ *
+ * 체크 표시가 가능한 버튼 형태의 UI 컴포넌트입니다.
+ * 버튼은 기본 상태(default)와 활성화 상태(active)를 지원하며,
+ * 크기 설정을 통해 다양한 스타일을 적용할 수 있습니다.
+ *
+ * @component
+ * @example
+ * <Chip variant="default" size="sm">Default Chip</Chip>
+ * <Chip variant="active" size="lg">Active Chip</Chip>
+ *
+ * @param {Object} props - 버튼에 전달되는 속성
+ * @param {'default' | 'active'} [props.variant='default'] - 버튼의 상태를 결정하는 variant 값
+ *    - 'default': 기본 상태
+ *    - 'active': 활성화된 상태로 체크 표시가 나타남
+ * @param {'sm' | 'lg'} [props.size='sm'] - 버튼의 크기 설정
+ *    - 'sm': 작은 크기
+ *    - 'lg': 큰 크기
+ * @param {React.ReactNode} props.children - 버튼에 표시될 텍스트나 다른 JSX 요소
+ * @param {React.ButtonHTMLAttributes} props.otherProps - 버튼의 HTML 속성들 (예: onClick, disabled 등)
+ *
+ * @returns {JSX.Element} - Chip 버튼 컴포넌트
+ */
+
 const Chip: React.FC<ChipProps> = ({
   className,
   variant = 'default',
