cf968a844983464675b6fd94c8301638f0a74211
beom <gb3187@naver.com>
2024-11-27 18:18:25 +0900

    Feat: 모달 컨테이너 생성

--- src/components/common/Modal/TodoModal/TodoModal.tsx
+++ src/components/common/Modal/TodoModal/TodoModal.tsx
@@ -1,3 +1,4 @@
+import ModalContainer from '../ModalContainer/ModalContainer';
 import TodoModalBtn from './TodoModalBtn';
 import TodoModalDocs from './TodoModalDocs';
 import TodoModalHeader from './TodoModalHeader';
@@ -6,7 +7,7 @@ import TodoModalTitle from './TodoModalTitle';
 
 const TodoModal = () => {
   return (
-    <div className="fixed inset-0 z-50 flex items-center justify-center bg-black/80">
+    <ModalContainer>
       <div className="flex size-full flex-col items-start gap-10 bg-white px-16 py-24 sm:h-676 sm:w-520 sm:rounded-12 sm:p-24">
         <div className="flex flex-1 flex-col items-start gap-40 self-stretch">
           <div className="flex flex-col gap-24 self-stretch">
@@ -18,7 +19,7 @@ const TodoModal = () => {
           <TodoModalBtn />
         </div>
       </div>
-    </div>
+    </ModalContainer>
   );
 };
 
