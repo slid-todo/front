22fa96bf74d394e09f59a8e7e00ae93570876184
dudwns0213 <dudwns0213@gmail.com>
2024-11-27 14:09:08 +0900

    Test: Pretendard, fontSize test

--- src/app/(route)/page.tsx
+++ src/app/(route)/page.tsx
@@ -5,6 +5,12 @@ export default function Home() {
     <div>
       <button className="size-100">안녕asdfasfd</button>
       <HeartIcon width="32" height="32" fill="#FF0000" />
+
+      <div className="text-3xl-bold">3xl bold</div>
+      <div className="text-3xl-semibold">3xl semibold</div>
+      <div className="text-3xl-medium">3xl medium</div>
+      <div className="text-3xl-normal">3xl normal</div>
+      <div className="text-3xl-light">3xl light</div>
     </div>
   );
 }
