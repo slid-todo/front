82d38b266c5df445506f77f439a0f47c4deb7ac4
beom <gb3187@naver.com>
2024-11-27 18:40:37 +0900

    Feat: 모달 외부 클릭, X 클릭 시 닫힘 기능 구현

--- src/components/common/Modal/TodoModal/TodoModalHeader.tsx
+++ src/components/common/Modal/TodoModal/TodoModalHeader.tsx
@@ -1,9 +1,19 @@
+'use client';
 import { IoMdClose } from 'react-icons/io';
-const TodoModalHeader = () => {
+
+interface TodoModalHeaderProps {
+  onClose: () => void;
+}
+
+const TodoModalHeader = ({ onClose }: TodoModalHeaderProps) => {
+  const handleClose = () => {
+    onClose();
+  };
+
   return (
     <div className="flex items-center justify-between">
       <span className="text-lg-bold text-slate-800">할 일 생성</span>
-      <IoMdClose size={24} className="cursor-pointer" />
+      <IoMdClose size={24} className="cursor-pointer" onClick={handleClose} />
     </div>
   );
 };
