4cbab144d4218ccaf6b096235f4051545a552b66
beom <gb3187@naver.com>
2024-11-27 15:05:59 +0900

    Feat: TodoModal 퍼블리싱 초안 디자인

--- src/app/(route)/page.tsx
+++ src/app/(route)/page.tsx
@@ -1,10 +1,12 @@
 import HeartIcon from '@/assets/icon-heart.svg';
+import TodoModal from '@/components/common/Modal/TodoModal';
 
 export default function Home() {
   return (
     <div>
       <button className="size-100">안녕asdfasfd</button>
       <HeartIcon width="32" height="32" fill="#FF0000" />
+      <TodoModal />
     </div>
   );
 }
