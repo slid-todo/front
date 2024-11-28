0082210692db8673b4951ed2ce36ea5955e1e77e
dudwns0213 <dudwns0213@gmail.com>
2024-11-28 11:45:35 +0900

    Feat: create Filter stories

---
+++ src/components/common/Filter/index.stories.ts
@@ -0,0 +1,24 @@
+import { Meta, StoryObj } from '@storybook/react';
+import { Filter } from '.';
+
+const meta: Meta<typeof Filter> = {
+  title: 'Components/Filter',
+  component: Filter,
+  parameters: {
+    layout: 'centered',
+  },
+  tags: ['autodocs'],
+  argTypes: {
+    filters: {
+      options: ['default', 'active'],
+    },
+  },
+};
+
+export default meta;
+
+export const Default: StoryObj<typeof Filter> = {
+  args: {
+    filters: ['All', 'To Do', 'Completed', 'In Progress'],
+  },
+};
