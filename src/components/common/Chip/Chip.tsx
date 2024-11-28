55a34506108eba2d19e36d634118d6ba370512a9
ejinn1 <ejinn1@naver.com>
2024-11-28 10:17:54 +0900

    Docs: change chip component docs

--- src/components/common/Chip/Chip.tsx
+++ src/components/common/Chip/Chip.tsx
@@ -24,25 +24,24 @@ export interface ChipProps
  * Chip 컴포넌트
  *
  * 체크 표시가 가능한 버튼 형태의 UI 컴포넌트입니다.
- * 버튼은 기본 상태(default)와 활성화 상태(active)를 지원하며,
- * 크기 설정을 통해 다양한 스타일을 적용할 수 있습니다.
+ * 이 컴포넌트는 `variant`와 `size`를 통해 다양한 상태와 스타일을 제공하며,
+ * 버튼 안에 자식 요소(`children`)를 포함할 수 있습니다.
+ *
+ * `variant` 값에 따라 버튼이 기본 상태(`default`) 또는 활성화 상태(`active`)를 가질 수 있으며,
+ * 활성화 상태에서는 체크 아이콘이 표시됩니다.
+ * 크기(`size`) 옵션을 통해 작은 크기(`sm`)와 큰 크기(`lg`)를 선택할 수 있습니다.
+ *
+ * 추가적으로, 버튼 속성(`onClick`, `disabled` 등)을 전달하여 HTML 버튼의 기본 기능을 사용할 수 있습니다.
  *
  * @component
  * @example
+ * // 기본 상태 버튼
  * <Chip variant="default" size="sm">Default Chip</Chip>
+ *
+ * // 활성화 상태 버튼
  * <Chip variant="active" size="lg">Active Chip</Chip>
  *
- * @param {Object} props - 버튼에 전달되는 속성
- * @param {'default' | 'active'} [props.variant='default'] - 버튼의 상태를 결정하는 variant 값
- *    - 'default': 기본 상태
- *    - 'active': 활성화된 상태로 체크 표시가 나타남
- * @param {'sm' | 'lg'} [props.size='sm'] - 버튼의 크기 설정
- *    - 'sm': 작은 크기
- *    - 'lg': 큰 크기
- * @param {React.ReactNode} props.children - 버튼에 표시될 텍스트나 다른 JSX 요소
- * @param {React.ButtonHTMLAttributes} props.otherProps - 버튼의 HTML 속성들 (예: onClick, disabled 등)
- *
- * @returns {JSX.Element} - Chip 버튼 컴포넌트
+ * @returns {JSX.Element} Chip 컴포넌트의 JSX 출력
  */
 
 export const Chip: React.FC<ChipProps> = ({
