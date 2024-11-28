d95dd876a74dbbba1c9b15898c8750e61a63f35a
beom <gb3187@naver.com>
2024-11-28 13:30:51 +0900

    Feat: 할일 생성 및 수정 모달 구분할 props 생성

--- src/app/(route)/GiBeom/page.tsx
+++ src/app/(route)/GiBeom/page.tsx
@@ -4,19 +4,31 @@ import TodoModal from '@/components/common/Modal/TodoModal';
 
 export default function GiBeom() {
   const [isOpen, setIsOpen] = useState(true);
+  const [todoType, setTodoType] = useState('');
 
   const handleClose = () => {
     setIsOpen(false);
   };
 
-  const handleOpen = () => {
+  const handleOpenTypeAssign = () => {
+    setTodoType('생성');
+    setIsOpen(true);
+  };
+
+  const handleOpenTypeModify = () => {
+    setTodoType('수정');
     setIsOpen(true);
   };
 
   return (
     <div>
-      <button onClick={handleOpen}>열어</button>
-      {isOpen ? <TodoModal onClose={handleClose} /> : <></>}
+      <button className="bg-blue-700" onClick={handleOpenTypeAssign}>
+        할일 생성 열어
+      </button>
+      <button className="bg-red-700" onClick={handleOpenTypeModify}>
+        할일 수정 열어
+      </button>
+      {isOpen ? <TodoModal onClose={handleClose} todoType={todoType} /> : <></>}
     </div>
   );
 }
