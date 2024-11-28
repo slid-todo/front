3c852e5e9e2e73563dfe5952573abd38d05af5b8
beom <gb3187@naver.com>
2024-11-28 13:15:21 +0900

    Feat: Done 버튼 추가

--- src/components/common/Modal/TodoModal/TodoModalHeader/index.tsx
+++ src/components/common/Modal/TodoModal/TodoModalHeader/index.tsx
@@ -1,19 +1,39 @@
 'use client';
+import { useState } from 'react';
 import { IoMdClose } from 'react-icons/io';
+import { FaCheck } from 'react-icons/fa6';
 
 interface TodoModalHeaderProps {
   onClose: () => void;
 }
 
 export const TodoModalHeader = ({ onClose }: TodoModalHeaderProps) => {
+  const [isChkClick, setIstChkClick] = useState(false);
+
   const handleClose = () => {
     onClose();
   };
 
+  const handleClick = () => {
+    setIstChkClick(!isChkClick);
+  };
+
   return (
-    <div className="flex items-center justify-between">
-      <span className="text-lg-bold text-slate-800">할 일 생성</span>
-      <IoMdClose size={24} className="cursor-pointer" onClick={handleClose} />
+    <div className="flex flex-col gap-8 self-stretch">
+      <div className="flex items-center justify-between">
+        <span className="text-lg-bold text-slate-800">할 일 생성</span>
+        <IoMdClose size={24} className="cursor-pointer" onClick={handleClose} />
+      </div>
+      <div className="flex items-center gap-6">
+        <button
+          className={`flex size-18 items-start justify-center rounded-6 border border-slate-200 ${isChkClick ? 'bg-blue-600' : 'bg-white'} `}
+          onClick={handleClick}
+        >
+          {isChkClick ? <FaCheck className="size-16 text-white" /> : <></>}
+        </button>
+
+        <span className="text-base-semibold text-slate-600">Done</span>
+      </div>
     </div>
   );
 };
