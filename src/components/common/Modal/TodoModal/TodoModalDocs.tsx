6b35652267e28aadd61fe2122c59867fd9271d50
beom <gb3187@naver.com>
2024-11-28 11:06:27 +0900

    Feat: Chip 컴포넌트 적용 및 토글 효과

--- src/components/common/Modal/TodoModal/TodoModalDocs.tsx
+++ src/components/common/Modal/TodoModal/TodoModalDocs.tsx
@@ -1,18 +1,35 @@
+'use client';
+import { useState } from 'react';
 import { FaPlus } from 'react-icons/fa';
+import { Chip } from '../../Chip/Chip';
 
 export const TodoModalDocs = () => {
+  const [isFileActive, setIsFileActive] = useState(true);
+  const [isLinkActive, setIsLinkActive] = useState(false);
+
+  const onClickChip = () => {
+    setIsFileActive(!isFileActive);
+    setIsLinkActive(!isLinkActive);
+  };
+
   return (
     <div className="flex flex-col items-start gap-12">
       {/* 자료 */}
       <div className="flex flex-col items-start gap-12">
         <span className="text-base-semibold text-slate-800">자료</span>
         <div className="flex items-start gap-12">
-          <button className="flex items-start gap-10 rounded-8 bg-slate-900 py-8 pl-8 pr-12 text-sm-medium text-white sm:text-base-medium">
-            파일업로드
-          </button>
-          <button className="flex items-start gap-10 rounded-8 bg-slate-100 py-8 pl-8 pr-12 text-sm-medium text-slate-800 sm:text-base-medium">
+          <Chip
+            variant={isFileActive ? 'active' : 'default'}
+            onClick={onClickChip}
+          >
+            파일 업로드
+          </Chip>
+          <Chip
+            variant={isLinkActive ? 'active' : 'default'}
+            onClick={onClickChip}
+          >
             링크 첨부
-          </button>
+          </Chip>
         </div>
       </div>
       {/* 자료 */}
@@ -20,7 +37,6 @@ export const TodoModalDocs = () => {
       <div className="flex h-184 w-full shrink-0 items-center justify-center rounded-12 bg-slate-50">
         <button className="inline-flex flex-col items-center justify-center gap-8">
           <FaPlus size={24} className="text-slate-400" />
-          {/* 크기 조정 가능 */}
           <span className="text-sm-normal text-slate-400 sm:text-base-normal">
             파일을 업로드해주세요
           </span>
