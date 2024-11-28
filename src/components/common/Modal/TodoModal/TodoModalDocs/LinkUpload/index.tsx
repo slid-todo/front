fe4467ec5d1aa467eaea7da19701ddb235854097
beom <gb3187@naver.com>
2024-11-28 11:30:23 +0900

    Feat: 파일업로드 및 링크 첨부 컴포넌트 생성

---
+++ src/components/common/Modal/TodoModal/TodoModalDocs/LinkUpload/index.tsx
@@ -0,0 +1,19 @@
+import { FaLink } from 'react-icons/fa';
+
+export const LinkUpload = () => {
+  return (
+    <div className="flex h-184 w-full shrink-0 items-center justify-center rounded-12 bg-slate-50">
+      <div className="inline-flex flex-col items-center justify-center gap-8">
+        <FaLink size={24} className="text-slate-400" />
+        <span className="text-sm-normal text-slate-400 sm:text-base-normal">
+          링크를 첨부해주세요
+        </span>
+        <input
+          type="url"
+          placeholder="링크를 입력하세요"
+          className="mt-4 w-full max-w-[500px] rounded-8 border px-4 py-2 text-sm text-slate-600" // 길이 늘림
+        />
+      </div>
+    </div>
+  );
+};
