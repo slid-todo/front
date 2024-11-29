fc3eb5d5c03452e221098ab3d01be6ad60f7109d
beom <gb3187@naver.com>
2024-11-29 13:57:29 +0900

    Feat: integrate dropdown component with TodoModalTarget and add basic styling

--- src/components/common/Dropdown/Dropdown.tsx
+++ src/components/common/Dropdown/Dropdown.tsx
@@ -4,11 +4,20 @@ interface DropdownProps {
 
 export const Dropdown = ({ dropdownData }: DropdownProps) => {
   return (
-    <ul className="absolute inline-flex w-full flex-col items-start rounded-12 bg-white shadow-lg">
+    <ul className="absolute top-full inline-flex max-h-150 w-full flex-col items-start overflow-y-auto rounded-12 bg-white shadow-lg">
+      <style jsx>{`
+        ul::-webkit-scrollbar {
+          width: 0px; /* 스크롤바 너비를 0으로 설정 */
+        }
+        ul {
+          scrollbar-width: thin; /* Firefox에서 스크롤바를 얇게 설정 */
+          scrollbar-color: transparent transparent; /* 스크롤바 색상을 투명으로 설정 */
+        }
+      `}</style>
       {dropdownData.map((item) => {
         return (
           <li
-            className="flex-center w-full p-10 text-sm-normal hover:bg-slate-200 sm:text-lg-normal"
+            className="flex-center mt-2 w-full cursor-pointer rounded-12 p-10 text-sm-normal hover:bg-slate-200 sm:text-lg-normal"
             key={item}
           >
             {item}
--- src/components/common/Modal/TodoModal/TodoModalTarget/index.tsx
+++ src/components/common/Modal/TodoModal/TodoModalTarget/index.tsx
@@ -1,17 +1,44 @@
-import { FaSortDown } from 'react-icons/fa';
+import { useState } from 'react';
+import { IoMdArrowDropdown, IoMdArrowDropup } from 'react-icons/io';
 import { PLACEHOLDERS } from '@/constants/Placeholders';
+import { cn } from '@/utils/className';
+import { Dropdown } from '@/components/common/Dropdown/Dropdown';
+import { TODO_MOCK_DATA } from '@/constants/TodoMockData';
 
 export const TodoModalTarget = () => {
+  const [isOpenDropdown, setIsOpenDropdown] = useState(false);
+
+  const dropdownIconClass = cn('size-24 cursor-pointer');
+
+  const handleOpenDropdown = () => {
+    setIsOpenDropdown(true);
+  };
+
+  const handleCloseDropdown = () => {
+    setIsOpenDropdown(false);
+  };
+
   return (
-    <div className="flex flex-col items-start gap-12 self-stretch">
+    <div className="relative flex flex-col items-start gap-12 self-stretch">
       <span className="text-base-semibold text-slate-800">목표</span>
-      <div className="flex items-center justify-between self-stretch rounded-12 bg-slate-50 px-20 py-12 text-sm-normal sm:text-base-normal">
+      <div className=" flex items-center justify-between self-stretch rounded-12 bg-slate-50 px-20 py-12 text-sm-normal sm:text-base-normal ">
         <input
           className="bg-slate-50 focus:outline-none"
           placeholder={PLACEHOLDERS.TARGET}
         />
-        <FaSortDown className="size-24 cursor-pointer" />
+        {isOpenDropdown ? (
+          <IoMdArrowDropdown
+            className={dropdownIconClass}
+            onClick={handleCloseDropdown}
+          />
+        ) : (
+          <IoMdArrowDropup
+            className={dropdownIconClass}
+            onClick={handleOpenDropdown}
+          />
+        )}
       </div>
+      {isOpenDropdown ? <Dropdown dropdownData={TODO_MOCK_DATA} /> : <></>}
     </div>
   );
 };
