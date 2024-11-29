f35746ff035ac48e97b9d8d74d6768855b0ba4c4
beom <gb3187@naver.com>
2024-11-29 15:48:34 +0900

    Setting: scrollbar hide setting

--- src/components/common/Dropdown/index.tsx
+++ src/components/common/Dropdown/index.tsx
@@ -32,16 +32,7 @@ export const Dropdown = ({ dropdownData, onSelectItem }: DropdownProps) => {
   };
 
   return (
-    <ul className="absolute top-full inline-flex max-h-150 w-full flex-col items-start overflow-y-auto rounded-12 bg-white shadow-lg">
-      <style jsx>{`
-        ul::-webkit-scrollbar {
-          width: 0px; /* 스크롤바 너비를 0으로 설정 */
-        }
-        ul {
-          scrollbar-width: none; /* Firefox에서 스크롤바를 얇게 설정 */
-          scrollbar-color: transparent transparent; /* 스크롤바 색상을 투명으로 설정 */
-        }
-      `}</style>
+    <ul className="absolute top-full inline-flex max-h-150 w-full flex-col items-start overflow-y-auto rounded-12 bg-white shadow-lg scrollbar-hide">
       {dropdownData.map((item) => {
         return (
           <li
