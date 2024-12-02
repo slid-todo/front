21d0e06ff756ff38193406fcdbe1e51d16d5a6f6
beom <gb3187@naver.com>
2024-12-02 15:17:52 +0900

    Feat: AuthFooter props 설정

--- src/components/specific-feature/AuthPage/AuthFooter/index.tsx
+++ src/components/specific-feature/AuthPage/AuthFooter/index.tsx
@@ -1,13 +1,21 @@
 import Link from 'next/link';
 
-export const AuthFooter = () => {
+interface AuthFooterProps {
+  description: string;
+  linkToDescription: string;
+  linkTo: string;
+}
+
+export const AuthFooter = ({
+  description,
+  linkToDescription,
+  linkTo,
+}: AuthFooterProps) => {
   return (
     <div className="flex items-start gap-4">
-      <span className="text-sm-medium text-slate-800">
-        찍찍이가 처음이신가요?
-      </span>
-      <Link className="text-sm-medium text-blue-500 underline" href="/signup">
-        회원가입
+      <span className="text-sm-medium text-slate-800">{description}</span>
+      <Link className="text-sm-medium text-blue-500 underline" href={linkTo}>
+        {linkToDescription}
       </Link>
     </div>
   );
