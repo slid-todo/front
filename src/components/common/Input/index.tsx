bbdb5594e16b65375d31cc188432afb24f3b28fd
ejinn1 <ejinn1@naver.com>
2024-12-02 15:13:41 +0900

    Refactor: remove import React

--- src/components/common/Input/index.tsx
+++ src/components/common/Input/index.tsx
@@ -1,10 +1,10 @@
-import React from 'react';
+import { forwardRef } from 'react';
 
 import { cn } from '@/utils/className';
 
 export type InputProps = React.InputHTMLAttributes<HTMLInputElement>;
 
-export const Input = React.forwardRef<HTMLInputElement, InputProps>(
+export const Input = forwardRef<HTMLInputElement, InputProps>(
   ({ className, type = 'text', ...props }, ref) => {
     const inputClass = cn(
       'w-full px-24 py-12 outline-none bg-white rounded-12 text-sm-normal md:text-base-normal',
