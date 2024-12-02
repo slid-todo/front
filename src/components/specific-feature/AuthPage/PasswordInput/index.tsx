41de746a5b96efbd97c9f16b74f902a5b28ae7e8
beom <gb3187@naver.com>
2024-12-02 15:41:19 +0900

    Feat: 이메일 비밀번호Placeholder 상수화

--- src/components/specific-feature/AuthPage/PasswordInput/index.tsx
+++ src/components/specific-feature/AuthPage/PasswordInput/index.tsx
@@ -1,12 +1,13 @@
 import { MdVisibilityOff } from 'react-icons/md';
 import { Input } from '@/components/common/Input';
+import { PLACEHOLDERS } from '@/constants/Placeholders';
 
 export const PasswordInput = () => {
   return (
     <div className="flex w-full flex-col items-start gap-12">
       <span className="text-base-semibold text-slate-800 ">비밀번호</span>
       <div className="flex w-full items-center justify-between gap-10 pr-24">
-        <Input type="password" placeholder="비밀번호를 입력해주세요" />
+        <Input type="password" placeholder={PLACEHOLDERS.PASSWORD} />
         <MdVisibilityOff className="size-24 cursor-pointer" />
       </div>
     </div>
