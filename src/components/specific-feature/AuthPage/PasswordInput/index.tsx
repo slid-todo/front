e69daf3801b6a6c907cac08b2050192bc8e3ff2e
beom <gb3187@naver.com>
2024-12-02 15:37:35 +0900

    Feat: 로그인 페이지 input 공통 컴포넌트 적용

--- src/components/specific-feature/AuthPage/PasswordInput/index.tsx
+++ src/components/specific-feature/AuthPage/PasswordInput/index.tsx
@@ -1,14 +1,12 @@
 import { MdVisibilityOff } from 'react-icons/md';
+import { Input } from '@/components/common/Input';
 
 export const PasswordInput = () => {
   return (
     <div className="flex w-full flex-col items-start gap-12">
       <span className="text-base-semibold text-slate-800 ">비밀번호</span>
-      <div className="flex w-full items-center justify-between gap-10 px-24 py-12">
-        <input
-          className="w-full outline-none"
-          placeholder="비밀번호를 입력해주세요"
-        />
+      <div className="flex w-full items-center justify-between gap-10 pr-24">
+        <Input type="password" placeholder="비밀번호를 입력해주세요" />
         <MdVisibilityOff className="size-24 cursor-pointer" />
       </div>
     </div>
