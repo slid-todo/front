632d30720fb24f98795434c0e4f0173fc4e2ff63
beom <gb3187@naver.com>
2024-12-02 15:37:35 +0900

    Feat: 로그인 페이지 input 공통 컴포넌트 적용

--- src/components/specific-feature/AuthPage/EmailInput/index.tsx
+++ src/components/specific-feature/AuthPage/EmailInput/index.tsx
@@ -1,13 +1,10 @@
+import { Input } from '@/components/common/Input';
+
 export const EmailInput = () => {
   return (
     <div className="flex w-full flex-col items-start gap-12">
       <span className="text-base-semibold text-slate-800 ">아이디</span>
-      <div className="flex w-full items-center gap-10 px-24 py-12">
-        <input
-          className="w-full outline-none"
-          placeholder="이메일을 입력해주세요"
-        />
-      </div>
+      <Input type="text" placeholder="이메일을 입력해주세요" />
     </div>
   );
 };
