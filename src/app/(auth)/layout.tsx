e9634f1f2519da557033bf19ee6c7d577d58d973
beom <gb3187@naver.com>
2024-12-04 13:28:45 +0900

    Feat: 최상단 layout 파일 생성 및 하위 layout 파일 수정

--- src/app/(auth)/layout.tsx
+++ src/app/(auth)/layout.tsx
@@ -1,26 +1,7 @@
-import localFont from 'next/font/local';
-import QueryProvider from '@/provider/QueryProvider';
-import '@/styles/globals.css';
-
-const pretendard = localFont({
-  src: '../../fonts/PretendardVariable.woff2',
-  display: 'swap',
-  weight: '45 920',
-  variable: '--font-pretendard',
-});
-
 export default function RootLayout({
   children,
 }: Readonly<{
   children: React.ReactNode;
 }>) {
-  return (
-    <html lang="ko">
-      <body className={`${pretendard.variable} font-pretendard`}>
-        <QueryProvider>
-          <div className="flex-center min-h-screen">{children}</div>
-        </QueryProvider>
-      </body>
-    </html>
-  );
+  return <div className="flex-center min-h-screen">{children}</div>;
 }
--- src/app/(route)/layout.tsx
+++ src/app/(route)/layout.tsx
@@ -1,17 +1,5 @@
 import type { Metadata } from 'next';
-
-import localFont from 'next/font/local';
-
 import { Sidebar } from '@/components/Sidebar';
-import QueryProvider from '@/provider/QueryProvider';
-import '@/styles/globals.css';
-
-const pretendard = localFont({
-  src: '../../fonts/PretendardVariable.woff2',
-  display: 'swap',
-  weight: '45 920',
-  variable: '--font-pretendard',
-});
 
 export const metadata: Metadata = {
   title: 'Create Next App',
@@ -24,13 +12,9 @@ export default function RootLayout({
   children: React.ReactNode;
 }>) {
   return (
-    <html lang="ko">
-      <body className={`${pretendard.variable} font-pretendard`}>
-        <QueryProvider>
-          <Sidebar />
-          {children}
-        </QueryProvider>
-      </body>
-    </html>
+    <>
+      <Sidebar />
+      {children}
+    </>
   );
 }
