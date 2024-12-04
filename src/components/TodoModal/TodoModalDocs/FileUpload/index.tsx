11b82cf61ed0265c883d946f7f6bd1807b0f4efa
beom <gb3187@naver.com>
2024-12-04 15:12:16 +0900

    Refactor: Input 공통 컴포넌트 적용

--- src/components/TodoModal/TodoModalDocs/FileUpload/index.tsx
+++ src/components/TodoModal/TodoModalDocs/FileUpload/index.tsx
@@ -3,7 +3,7 @@ import { PLACEHOLDERS } from '@/constants/Placeholders';
 
 export const FileUpload = () => {
   return (
-    <div className="flex h-184 w-full shrink-0 items-center justify-center rounded-12 bg-slate-50">
+    <div className="flex h-184 w-full shrink-0 items-center justify-center rounded-12 bg-white">
       <button className="inline-flex flex-col items-center justify-center gap-8">
         <FaPlus className="size-24 text-slate-400" />
         <span className="text-sm-normal text-slate-400 sm:text-base-normal">
--- src/components/TodoModal/TodoModalDocs/LinkUpload/index.tsx
+++ src/components/TodoModal/TodoModalDocs/LinkUpload/index.tsx
@@ -1,20 +1,6 @@
-import { FaLink } from 'react-icons/fa';
 import { PLACEHOLDERS } from '@/constants/Placeholders';
+import { Input } from '@/components/common/Input';
 
 export const LinkUpload = () => {
-  return (
-    <div className="flex h-184 w-full shrink-0 items-center justify-center rounded-12 bg-slate-50">
-      <div className="inline-flex flex-col items-center justify-center gap-8">
-        <FaLink className="size-24 text-slate-400" />
-        <span className="text-sm-normal text-slate-400 sm:text-base-normal">
-          {PLACEHOLDERS.LINK_ATTACH}
-        </span>
-        <input
-          type="url"
-          placeholder={PLACEHOLDERS.LINK_INPUT}
-          className="mt-4 w-full max-w-500 rounded-8 border px-4 py-2 text-sm text-slate-600 focus:outline-none" // 길이 늘림
-        />
-      </div>
-    </div>
-  );
+  return <Input type="url" placeholder={PLACEHOLDERS.LINK_INPUT} />;
 };
