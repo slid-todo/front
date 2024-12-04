2479b7adbfd6906edd1f87ce3601620cd1f80111
beom <gb3187@naver.com>
2024-12-04 18:01:15 +0900

    Feat: 파일 업로드 관련 로직 작성 (제목만 뜨게끔)

--- src/components/TodoModal/TodoModalBtn/index.tsx
+++ src/components/TodoModal/TodoModalBtn/index.tsx
@@ -1,6 +1,6 @@
 export const TodoModalBtn = () => {
   return (
-    <button className="mt-auto flex w-full items-center justify-center gap-10 self-stretch rounded-12 bg-slate-400 py-12 sm:mt-0">
+    <button className="mt-auto flex w-full items-center justify-center gap-10 self-stretch rounded-12 bg-primary-200 py-12 sm:mt-0">
       <span className="text-base-semibold text-white">확인</span>
     </button>
   );
--- src/components/TodoModal/TodoModalDocs/FileUpload/index.tsx
+++ src/components/TodoModal/TodoModalDocs/FileUpload/index.tsx
@@ -1,15 +1,42 @@
+import { ChangeEvent, useRef, useState } from 'react';
+import { GrDocument } from 'react-icons/gr';
 import { FaPlus } from 'react-icons/fa';
 import { PLACEHOLDERS } from '@/constants/Placeholders';
 
 export const FileUpload = () => {
+  const [fileName, setFileName] = useState<string | null>(null);
+  const fileUploadRef = useRef<HTMLInputElement>(null);
+
+  const handleFileName = (e: ChangeEvent<HTMLInputElement>) => {
+    const selectedFile = e.target.files?.[0];
+    if (selectedFile) {
+      setFileName(selectedFile.name);
+    }
+  };
+
   return (
     <div className="flex h-184 w-full shrink-0 items-center justify-center rounded-12 bg-white">
-      <button className="inline-flex flex-col items-center justify-center gap-8">
-        <FaPlus className="size-24 text-slate-400" />
+      <button
+        className="inline-flex flex-col items-center justify-center gap-8"
+        onClick={() => fileUploadRef.current?.click()}
+      >
+        {fileName ? (
+          <GrDocument className="size-24 text-slate-400" />
+        ) : (
+          <FaPlus className="size-24 text-slate-400" />
+        )}
         <span className="text-sm-normal text-slate-400 sm:text-base-normal">
-          {PLACEHOLDERS.FILE}
+          {fileName ? fileName : PLACEHOLDERS.FILE}
         </span>
       </button>
+      <input
+        id="file-input"
+        ref={fileUploadRef}
+        className="hidden"
+        type={'file'}
+        accept="image/*"
+        onChange={handleFileName}
+      />
     </div>
   );
 };
