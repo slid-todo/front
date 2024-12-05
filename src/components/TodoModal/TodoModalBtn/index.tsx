2aab24c425c195783986248faa0a47c037a659bb
beom <gb3187@naver.com>
2024-12-05 13:45:26 +0900

    Refactor: TodoModal 공통 버튼 컴포넌트 적용

--- src/components/TodoModal/TodoModalBtn/index.tsx
+++ src/components/TodoModal/TodoModalBtn/index.tsx
@@ -1,7 +1,9 @@
+import { Button } from '@/components/common/Button/Button';
+
 export const TodoModalBtn = () => {
   return (
-    <button className="mt-auto flex w-full items-center justify-center gap-10 self-stretch rounded-12 bg-primary-200 py-12 sm:mt-0">
-      <span className="text-base-semibold text-white">확인</span>
-    </button>
+    <Button size="large" primary={true} className="mt-auto">
+      확인
+    </Button>
   );
 };
