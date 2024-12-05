8dd9bc8edd5b21b3a11df8e11d70149a214508b3
beom <gb3187@naver.com>
2024-12-05 17:59:48 +0900

    Fix: storybook 오류 수정 및 JSDoc 수정

--- src/components/common/Dropdown/index.tsx
+++ src/components/common/Dropdown/index.tsx
@@ -10,15 +10,14 @@ interface DropdownProps {
 /**
  * Dropdown 컴포넌트
  *
- * 사용자가 선택할 수 있는 항목들이 표시되는 드롭다운 메뉴입니다.
- * 이 컴포넌트는 `dropdownData`로 전달된 항목들을 리스트 형식으로 표시하며,
- * 각 항목을 클릭할 때 `onSelectItem` 콜백 함수가 실행됩니다.
+ * 사용자가 선택할 수 있는 항목들이 표시되는 드롭다운 메뉴 컴포넌트입니다.
  *
- * `dropdownData`는 드롭다운에 표시할 항목들을 담은 문자열 배열이며,
- * `onSelectItem`은 항목이 선택되었을 때 호출되는 콜백 함수입니다.
+ * `dropdownData`로 전달된 항목들을 리스트로 렌더링하며,
+ * 각 항목을 클릭하면 `onSelectItem` 콜백 함수가 호출됩니다.
  *
- * 추가적으로, 드롭다운 메뉴는 스크롤이 가능하며, 선택된 항목에 대한 스타일링을 제공합니다.
- * 메뉴 항목을 클릭하면 해당 항목이 선택되어, `onSelectItem` 함수로 전달됩니다.
+ * 드롭다운은 스크롤 가능하며, 선택된 항목은 시각적으로 구분됩니다.
+ *
+ * `isOpenDropdown` prop을 통해 드롭다운의 열림/닫힘 상태를 제어할 수 있습니다.
  *
  *
  * @component
@@ -26,6 +25,8 @@ interface DropdownProps {
  * @param {string[]} dropdownData - 드롭다운에 표시될 항목들을 담은 배열
  *
  * @param {(item: string) => void} onSelectItem - 항목이 선택되었을 때 호출되는 콜백 함수
+ *
+ * @param {boolean} isOpenDropdown - 드롭다운의 열림(true) 또는 닫힘(false) 상태를 제어하는 불리언 값.
  *
  * @returns {JSX.Element} 드롭다운 메뉴 JSX 출력
  */
