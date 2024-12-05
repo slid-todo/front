b63223f81561dab3845d05e83da1c07c756a5869
beom <gb3187@naver.com>
2024-12-05 17:59:48 +0900

    Fix: storybook 오류 수정 및 JSDoc 수정

--- src/components/common/Dropdown/index.stories.tsx
+++ src/components/common/Dropdown/index.stories.tsx
@@ -5,6 +5,7 @@ import { Dropdown } from '.';
 interface DropdownProps {
   dropdownData: string[];
   onSelectItem: (item: string) => void;
+  isOpenDropdown: boolean;
 }
 
 const meta: Meta<typeof Dropdown> = {
@@ -24,6 +25,12 @@ const meta: Meta<typeof Dropdown> = {
       action: 'selected',
       description: 'Callback function when an item is selected.',
     },
+    isOpenDropdown: {
+      control: {
+        type: 'boolean', // Boolean 타입으로 설정
+      },
+      description: 'Controls whether the dropdown is open or closed.',
+    },
   },
 };
 
@@ -35,27 +42,36 @@ export const Default: Story = {
   args: {
     dropdownData: ['Option 1', 'Option 2', 'Option 3', 'Option 4'],
     onSelectItem: (item: string) => console.log(`Selected item: ${item}`),
+    isOpenDropdown: false,
   },
   render: function Render(args: DropdownProps) {
     const [selectedItem, setSelectedItem] = useState<string | null>(null);
+    const [isOpen, setIsOpen] = useState<boolean>(args.isOpenDropdown);
 
     const handleSelectItem = (item: string) => {
       setSelectedItem(item);
       args.onSelectItem(item); // Trigger the Storybook action
     };
 
+    const toggleDropdown = () => {
+      setIsOpen(!isOpen);
+    };
+
     return (
       <div className="relative w-300">
-        {' '}
-        {/* Tailwind로 변환 */}
         <div className="mb-2 font-bold">
-          {' '}
-          {/* Tailwind로 변환 */}
           Selected Item: {selectedItem || 'None'}
         </div>
+        <button
+          className="mb-2 rounded bg-blue-500 px-4 py-2 text-white hover:bg-blue-600"
+          onClick={toggleDropdown}
+        >
+          Toggle Dropdown
+        </button>
         <Dropdown
           dropdownData={args.dropdownData}
           onSelectItem={handleSelectItem}
+          isOpenDropdown={isOpen} // 드롭다운 열림 상태 전달
         />
       </div>
     );
