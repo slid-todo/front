613f2fddc7852d5831ac6a9f7a3524593c4f3e5d
beom <gb3187@naver.com>
2024-12-05 16:04:26 +0900

    Refactor: 할일 모달 렌더링 시 애니메이션 적용

--- src/components/TodoModal/index.tsx
+++ src/components/TodoModal/index.tsx
@@ -1,3 +1,4 @@
+import { motion } from 'motion/react';
 import { TodoModalProps } from '@/types/TodoType';
 import { ModalContainer } from '@/components/common/Modal/ModalContainer';
 import { useTodoModalStore } from '@/store/useTodoModalStore';
@@ -9,6 +10,11 @@ import { TodoModalTitle } from './TodoModalTitle';
 import { TodoModalRepeat } from './TodoModalRepeat';
 
 const TodoModal = ({ todoType }: TodoModalProps) => {
+  const variants = {
+    hidden: { opacity: 0, scale: 0.8 },
+    visible: { opacity: 1, scale: 1, transition: { duration: 0.3 } },
+  };
+
   const { close } = useTodoModalStore();
 
   const handleClose = () => {
@@ -17,7 +23,12 @@ const TodoModal = ({ todoType }: TodoModalProps) => {
 
   return (
     <ModalContainer onClose={handleClose}>
-      <div className="flex size-full flex-col items-start gap-10 overflow-y-auto bg-custom-white-100 px-16 py-24 sm:h-auto sm:w-520 sm:rounded-12 sm:p-24">
+      <motion.div
+        variants={variants}
+        initial="hidden"
+        animate="visible"
+        className="flex size-full flex-col items-start gap-10 overflow-y-auto bg-custom-white-100 px-16 py-24 sm:h-auto sm:w-520 sm:rounded-12 sm:p-24"
+      >
         <div className="flex flex-1 flex-col items-start gap-40 self-stretch">
           <div className="flex flex-col gap-24 self-stretch">
             <TodoModalHeader todoType={todoType} onClose={handleClose} />
@@ -28,7 +39,7 @@ const TodoModal = ({ todoType }: TodoModalProps) => {
           </div>
           <TodoModalBtn />
         </div>
-      </div>
+      </motion.div>
     </ModalContainer>
   );
 };
