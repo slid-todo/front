d0117b2ea2af8878187ba572e350b54f6781ee6b
beom <gb3187@naver.com>
2024-12-06 16:48:55 +0900

    Feat: DayPicker 적용 및 기타 디자인 수정

--- src/components/TodoModal/TodoModalRepeat/index.tsx
+++ src/components/TodoModal/TodoModalRepeat/index.tsx
@@ -1,14 +1,12 @@
-import { PiTildeBold } from 'react-icons/pi';
+// import { PiTildeBold } from 'react-icons/pi';
 import { Date } from './Date';
 
 export const TodoModalRepeat = () => {
   return (
-    <div className="flex flex-col items-start gap-12 self-stretch">
+    <div className="relative flex flex-col items-start gap-12 self-stretch">
       <span className="text-base-semibold text-slate-800">기간</span>
       <div className="flex-center w-full gap-10">
-        <Date type="start" />
-        <PiTildeBold className="size-20" />
-        <Date type="end" />
+        <Date />
       </div>
     </div>
   );
