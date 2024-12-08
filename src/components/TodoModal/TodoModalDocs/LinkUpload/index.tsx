84bb1d548150ddf8a8c41cd7bc3dbd8c403b3579
beom <gb3187@naver.com>
2024-12-09 02:21:02 +0900

    Feat: 각 항목 별 함수 적용

--- src/components/TodoModal/TodoModalDocs/LinkUpload/index.tsx
+++ src/components/TodoModal/TodoModalDocs/LinkUpload/index.tsx
@@ -2,8 +2,15 @@ import { motion } from 'motion/react';
 import { PLACEHOLDERS } from '@/constants/Placeholders';
 import { Input } from '@/components/common/Input';
 import { todoModalVariants } from '@/utils/motionVariants';
+import { useTodoDataStore } from '@/store/useTodoDataStore';
 
 export const LinkUpload = () => {
+  const { link, setLink } = useTodoDataStore();
+
+  const handleLinkInput = (e: React.ChangeEvent<HTMLInputElement>) => {
+    setLink(e.target.value);
+  };
+
   return (
     <motion.div
       className="w-full"
@@ -11,7 +18,12 @@ export const LinkUpload = () => {
       initial="hidden"
       animate="visible"
     >
-      <Input type="url" placeholder={PLACEHOLDERS.LINK_INPUT} />
+      <Input
+        type="url"
+        placeholder={PLACEHOLDERS.LINK_INPUT}
+        value={link}
+        onChange={handleLinkInput}
+      />
     </motion.div>
   );
 };
--- src/components/TodoModal/TodoModalTitle/index.tsx
+++ src/components/TodoModal/TodoModalTitle/index.tsx
@@ -1,11 +1,22 @@
 import { Input } from '@/components/common/Input';
 import { PLACEHOLDERS } from '@/constants/Placeholders';
+import { useTodoDataStore } from '@/store/useTodoDataStore';
 
 export const TodoModalTitle = () => {
+  const { title, setTitle } = useTodoDataStore();
+
+  const handleInput = (e: React.ChangeEvent<HTMLInputElement>) => {
+    setTitle(e.target.value);
+  };
+
   return (
     <div className="flex flex-col items-start gap-12 self-stretch">
       <span className="text-base-semibold text-slate-800">제목</span>
-      <Input placeholder={PLACEHOLDERS.TITLE_TODO} />
+      <Input
+        placeholder={PLACEHOLDERS.TITLE_TODO}
+        value={title}
+        onChange={handleInput}
+      />
     </div>
   );
 };
