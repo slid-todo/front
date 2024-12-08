c98c39a5b9397d6979b930857f655530f2315a0e
beom <gb3187@naver.com>
2024-12-09 02:21:50 +0900

    Feat: reset 함수 적용 및 할일 데이터 유효성 검사 유틸함수로 분리

--- src/components/TodoModal/TodoModalBtn/index.tsx
+++ src/components/TodoModal/TodoModalBtn/index.tsx
@@ -1,8 +1,22 @@
 import { Button } from '@/components/common/Button/Button';
+import { useTodoDataStore } from '@/store/useTodoDataStore';
+import { todoDataValidation } from '@/utils/todoDataValidation';
 
 export const TodoModalBtn = () => {
+  const { title, target, date, fileName, link } = useTodoDataStore();
+
+  const viewData = () => {
+    console.log(title, target, date, fileName, link);
+  };
+
   return (
-    <Button size="large" primary={true} className="mt-auto">
+    <Button
+      size="large"
+      primary={true}
+      className="mt-auto"
+      onClick={viewData}
+      disabled={todoDataValidation(title, target, date)}
+    >
       확인
     </Button>
   );
