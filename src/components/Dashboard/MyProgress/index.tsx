912f7d48a6c9bb2dae97c4b1f6b47833fdfa0522
ejinn1 <ejinn1@naver.com>
2024-12-09 18:16:58 +0900

    Refactor: add my progress component

---
+++ src/components/Dashboard/MyProgress/index.tsx
@@ -0,0 +1,19 @@
+import { ProgressCircle } from '@/components/Dashboard/MyProgress/ProgressCircle';
+
+export interface MyProgressProps {
+  progressPercent: number;
+}
+
+export const MyProgress = ({ progressPercent }: MyProgressProps) => {
+  return (
+    <>
+      <p className="text-xl-semibold">내 진행 상황</p>
+      <div className="flex-center relative mt-32">
+        <ProgressCircle progressPercent={progressPercent} />
+        <p className="absolute text-3xl-bold text-primary-100">
+          {progressPercent} <span className="pb-4 text-base-medium">%</span>
+        </p>
+      </div>
+    </>
+  );
+};
