c275e0a3701210e36010d6fa2d0a6df1897fa3e2
dudwns0213 <dudwns0213@gmail.com>
2024-12-10 16:44:58 +0900

    Chore: todos mockdata 생성

---
+++ src/mocks/todoData.ts
@@ -0,0 +1,30 @@
+export const todos = [
+  {
+    id: 1,
+    title: '할 일 1',
+    goal: '목표 1',
+    status: 'In Progress',
+    createdAt: new Date('2024-12-01'),
+  },
+  {
+    id: 2,
+    title: '할 일 2',
+    goal: '목표 2',
+    status: 'Completed',
+    createdAt: new Date('2024-12-02'),
+  },
+  {
+    id: 3,
+    title: '할 일 3',
+    goal: '목표 1',
+    status: 'In Progress',
+    createdAt: new Date('2024-12-03'),
+  },
+  {
+    id: 4,
+    title: '할 일 4',
+    goal: '목표 3',
+    status: 'In Progress',
+    createdAt: new Date('2024-11-30'),
+  },
+];
