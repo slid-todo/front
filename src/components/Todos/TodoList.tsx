4b013bc7d71f0f29fb102543ad6dba1b98ef7b1a
dudwns0213 <dudwns0213@gmail.com>
2024-12-10 18:08:53 +0900

    Refactor: TodoList 중복 코드 삭제(TodoSection)

--- src/components/Todos/TodoList.tsx
+++ src/components/Todos/TodoList.tsx
@@ -1,8 +1,6 @@
-import React from 'react';
-import { FaPlus } from 'react-icons/fa6';
 import { Todo } from '@/types/Todos';
 import { TODO_EMPTY_STATE_MESSAGES } from '@/constants/Todos/TodoMessages';
-import { TodoItem } from './TodoItem';
+import { TodoSection } from './TodoSection';
 
 interface TodoListProps {
   inProgressTasks: Todo[];
@@ -13,61 +11,17 @@ export const TodoList = (props: TodoListProps) => {
   const { inProgressTasks, completedTasks } = props;
 
   return (
-    <div>
-      <div className="">
-        <div className="flex justify-between text-xs-bold text-custom-gray-100">
-          진행 중 ({inProgressTasks.length})
-          <div className="flex cursor-pointer items-center gap-2 text-sm-medium text-primary-100">
-            <FaPlus /> 할 일 추가
-          </div>
-        </div>
-        {inProgressTasks.length > 0 ? (
-          <div className="mt-12 space-y-2">
-            {inProgressTasks.map((task, index) => {
-              const isLastItem = index === inProgressTasks.length - 1;
-              return (
-                <TodoItem
-                  key={task.id}
-                  {...task}
-                  className={
-                    isLastItem ? '' : 'border-b border-custom-white-200'
-                  }
-                />
-              );
-            })}
-          </div>
-        ) : (
-          <div className="flex h-120 items-center justify-center text-sm-normal text-custom-gray-100">
-            {TODO_EMPTY_STATE_MESSAGES.IN_PROGRESS}
-          </div>
-        )}
-      </div>
-
-      <div className="mt-24">
-        <div className="flex justify-between text-xs-bold text-custom-gray-100">
-          완료 ({completedTasks.length})
-        </div>
-        {completedTasks.length > 0 ? (
-          <div className="mt-12 space-y-2">
-            {completedTasks.map((task, index) => {
-              const isLastItem = index === completedTasks.length - 1;
-              return (
-                <TodoItem
-                  key={task.id}
-                  {...task}
-                  className={
-                    isLastItem ? '' : 'border-b border-custom-white-200'
-                  }
-                />
-              );
-            })}
-          </div>
-        ) : (
-          <div className="flex h-120 items-center justify-center text-sm-normal text-custom-gray-100">
-            {TODO_EMPTY_STATE_MESSAGES.IN_PROGRESS}
-          </div>
-        )}
-      </div>
-    </div>
+    <>
+      <TodoSection
+        title="진행 중"
+        tasks={inProgressTasks}
+        emptyMessage={TODO_EMPTY_STATE_MESSAGES.IN_PROGRESS}
+      />
+      <TodoSection
+        title="완료"
+        tasks={completedTasks}
+        emptyMessage={TODO_EMPTY_STATE_MESSAGES.COMPLETED}
+      />
+    </>
   );
 };
