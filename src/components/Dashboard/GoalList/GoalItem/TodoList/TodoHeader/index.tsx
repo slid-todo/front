48e2a1a3e46663b5bab1b42007b6db1a4ba522c0
ejinn1 <ejinn1@naver.com>
2024-12-10 16:50:32 +0900

    Refactor: add animation todo list component and split header

---
+++ src/components/Dashboard/GoalList/GoalItem/TodoList/TodoHeader/index.tsx
@@ -0,0 +1,30 @@
+import { motion } from 'motion/react';
+import { FaAngleDown } from 'react-icons/fa6';
+import { TodoTypes } from '@/constants/DashboardMockData';
+import { formatDateToPoint } from '@/utils/date';
+
+interface TodoHeaderProps {
+  open: () => void;
+  todo: TodoTypes;
+  isOpen: boolean;
+}
+
+export const TodoHeader = ({ open, todo, isOpen }: TodoHeaderProps) => {
+  return (
+    <div onClick={open} className="relative my-8">
+      <p className="text-base-semibold">{todo.name}</p>
+      <p className="text-xs-medium leading-6 text-custom-gray-100">
+        {`${formatDateToPoint(todo.startDate)} - 
+          ${formatDateToPoint(todo.endDate)}`}
+      </p>
+      <motion.span
+        initial={{ rotate: 0 }}
+        animate={{ rotate: isOpen ? 180 : 0 }}
+        transition={{ duration: 0.3, ease: 'easeInOut' }}
+        className="absolute right-0 top-8"
+      >
+        <FaAngleDown className="size-24 p-4" style={{ strokeWidth: 15 }} />
+      </motion.span>
+    </div>
+  );
+};
--- src/components/Dashboard/GoalList/GoalItem/TodoList/index.tsx
+++ src/components/Dashboard/GoalList/GoalItem/TodoList/index.tsx
@@ -1,5 +1,9 @@
+'use client';
+
+import { useState } from 'react';
+import { AnimatePresence, motion } from 'motion/react';
 import { TodoTypes } from '@/constants/DashboardMockData';
-import { formatDateToPoint } from '@/utils/date';
+import { TodoHeader } from './TodoHeader';
 import { TodoItem } from './TodoItem';
 
 interface TodoListProps {
@@ -7,19 +11,30 @@ interface TodoListProps {
 }
 
 export const TodoList = ({ todo }: TodoListProps) => {
+  const [isOpen, setIsOpen] = useState(false);
+
+  const handleClickToggle = () => {
+    setIsOpen((prev) => !prev);
+  };
+
   return (
     <div key={todo.id} className="pb-16">
-      <div className="my-8">
-        <p className="text-base-semibold">{todo.name}</p>
-        <p className="text-xs-medium leading-6 text-custom-gray-100">
-          {formatDateToPoint(todo.startDate)} ~{formatDateToPoint(todo.endDate)}
-        </p>
-      </div>
-      <div className="mt-8 grid grid-cols-4 gap-x-4 gap-y-8">
-        {Array.from({ length: 11 }).map((_, index) => (
-          <TodoItem key={index} index={index} />
-        ))}
-      </div>
+      <TodoHeader open={handleClickToggle} todo={todo} isOpen={isOpen} />
+      <AnimatePresence>
+        {isOpen && (
+          <motion.div
+            className="mt-8 grid grid-cols-4 gap-x-4 gap-y-8"
+            initial={{ height: 0, opacity: 0 }}
+            animate={{ height: 'auto', opacity: 1 }}
+            exit={{ height: 0, opacity: 0 }}
+            transition={{ duration: 0.3, ease: 'easeInOut' }}
+          >
+            {Array.from({ length: 11 }).map((_, index) => (
+              <TodoItem key={index} index={index} />
+            ))}
+          </motion.div>
+        )}
+      </AnimatePresence>
     </div>
   );
 };
