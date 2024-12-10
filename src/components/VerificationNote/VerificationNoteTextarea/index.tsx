8d3f9dd44d77abce5f588c47810dfe2eec6d7ee3
beom <gb3187@naver.com>
2024-12-10 10:48:48 +0900

    Feat: 할일 상세 보기 리뷰 글자수 제한 (zustand)

--- src/components/VerificationNote/VerificationNoteTextarea/index.tsx
+++ src/components/VerificationNote/VerificationNoteTextarea/index.tsx
@@ -1,8 +1,18 @@
+import { ChangeEvent } from 'react';
+import { useVerificationNoteStore } from '@/store/useVerificationNoteStore';
+
 export const VerificationNoteTextarea = () => {
+  const { review, setReview } = useVerificationNoteStore();
+  const handleTextarea = (e: ChangeEvent<HTMLTextAreaElement>) => {
+    setReview(e.target.value);
+  };
+
   return (
     <textarea
       className="h-60 w-full resize-none bg-custom-white-100 outline-none scrollbar-hide"
       placeholder="이 할 일은 어땠나요?"
+      value={review}
+      onChange={handleTextarea}
     />
   );
 };
