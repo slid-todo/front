d2b15dbe0750212ba479393223cd0d67dc4299cc
dudwns0213 <dudwns0213@gmail.com>
2024-12-10 18:09:42 +0900

    Feat: create TodoContainer(Page 전체를 보여주는)

---
+++ src/components/Todos/TodoContainer.tsx
@@ -0,0 +1,44 @@
+import { useState } from 'react';
+import { Filter } from '@/components/common/Filter';
+import { TodoList } from '@/components/Todos';
+import { useFilteredTodos } from '@/hooks/useFilteredTodos';
+import { todos, goalFilters, sortFilters } from '@/mocks/todoData';
+
+export const TodoContainer = () => {
+  const [currentSortFilter, setCurrentSortFilter] = useState<string>('최신순');
+  const [currentGoalFilter, setCurrentGoalFilter] = useState<string>('전체');
+
+  const handleSortFilterChange = (filter: string) => {
+    setCurrentSortFilter(filter);
+  };
+
+  const handleGoalFilterChange = (filter: string) => {
+    setCurrentGoalFilter(filter);
+  };
+
+  const { inProgressTasks, completedTasks } = useFilteredTodos(
+    todos,
+    currentGoalFilter,
+    currentSortFilter,
+  );
+
+  return (
+    <div className="flex h-screen w-screen flex-col gap-16 bg-custom-white-100 px-16 pt-48">
+      <div className="mt-16 text-xl-bold text-custom-gray-300">내 할 일</div>
+      <Filter
+        filters={sortFilters}
+        onFilterChange={handleSortFilterChange}
+        className="space-x-8"
+      />
+      <Filter
+        filters={goalFilters}
+        onFilterChange={handleGoalFilterChange}
+        className="space-x-8"
+      />
+      <TodoList
+        inProgressTasks={inProgressTasks}
+        completedTasks={completedTasks}
+      />
+    </div>
+  );
+};
