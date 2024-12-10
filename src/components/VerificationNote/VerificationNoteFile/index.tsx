efc9b41d619f356c615640594c975f429329bb19
beom <gb3187@naver.com>
2024-12-10 11:31:11 +0900

    Feat: 첨부된 파일 컴포넌트 분리 및 조건 처리

---
+++ src/components/VerificationNote/VerificationNoteFile/index.tsx
@@ -0,0 +1,26 @@
+import { IoMdClose } from 'react-icons/io';
+
+interface VerificationNoteFileProps {
+  fileName: string;
+}
+
+export const VerificationNoteFile = ({
+  fileName,
+}: VerificationNoteFileProps) => {
+  return (
+    <div
+      className={`flex-col items-start gap-12 self-stretch ${fileName ? 'flex' : 'hidden'}`}
+    >
+      <span className="text-base-semibold">첨부된 파일</span>
+      <div className="flex items-center justify-between self-stretch">
+        <div className="flex items-center gap-8">
+          <div className="flex size-56 rounded-16 bg-fuchsia-500" />
+          <span className="text-sm-normal text-custom-gray-100">
+            {fileName}
+          </span>
+        </div>
+        <IoMdClose />
+      </div>
+    </div>
+  );
+};
