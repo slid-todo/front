68d90de1d3d03b31564fe9123ea701412c428dde
dudwns0213 <dudwns0213@gmail.com>
2024-12-10 17:54:41 +0900

    Style: TodoItem css

--- src/components/Todos/TodoItem.tsx
+++ src/components/Todos/TodoItem.tsx
@@ -1,15 +1,23 @@
+import { FaCamera } from 'react-icons/fa6';
+
 interface TodoItemProps {
   title: string;
   goal: string;
+  className?: string;
 }
 
 export const TodoItem = (props: TodoItemProps) => {
-  const { title, goal } = props;
+  const { title, goal, className } = props;
 
   return (
-    <div className="rounded border p-2">
-      <div>제목: {title}</div>
-      <div>목표: {goal}</div>
+    <div className={`flex h-72 ${className ? className : ''}`}>
+      <div className="my-8 flex size-56 items-center justify-center rounded-16 bg-sub-purple">
+        <FaCamera fill="white" />
+      </div>
+      <div className="my-14 ml-16">
+        <div className="text-xs-medium text-custom-gray-100">{goal}</div>
+        <div className="text-base-medium text-custom-gray-300">{title}</div>
+      </div>
     </div>
   );
 };
