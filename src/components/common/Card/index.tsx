96684d606220314e9acf5d3d025d8c57508a6bb3
ejinn1 <ejinn1@naver.com>
2024-12-11 13:55:39 +0900

    Feat: add Card component

---
+++ src/components/common/Card/index.tsx
@@ -0,0 +1,18 @@
+import { ReactNode } from 'react';
+
+import { cn } from '@/utils/className';
+
+interface CardProps {
+  children: ReactNode;
+  className?: string;
+  hasShadow?: boolean;
+}
+
+export const Card = ({ children, className }: CardProps) => {
+  const cardClass = cn(
+    'flex-center w-full flex-col gap-10 rounded-16 bg-white py-65',
+    className,
+  );
+
+  return <div className={cardClass}>{children}</div>;
+};
