c67fc0b17c01d227fcd4d1d0fb934fff5159bb08
ejinn1 <ejinn1@naver.com>
2024-12-11 15:20:26 +0900

    Refactor: add bell icon to header component

--- src/components/common/Header/index.tsx
+++ src/components/common/Header/index.tsx
@@ -1,6 +1,6 @@
 'use client';
 
-import { FaBarsStaggered } from 'react-icons/fa6';
+import { FaBarsStaggered, FaBell } from 'react-icons/fa6';
 import { useSidebarStore } from '@/store/useSidebarStore';
 
 interface HeaderProps {
@@ -11,12 +11,16 @@ export const Header = ({ title = '' }: HeaderProps) => {
   const { open } = useSidebarStore();
 
   return (
-    <div className="fixed left-0 top-0 z-10 flex h-48 w-full items-center bg-white px-16 md:hidden">
-      <FaBarsStaggered
-        className="w-24 cursor-pointer text-primary-100"
-        onClick={open}
-      />
-      <p className="ml-16 text-base-semibold">{title}</p>
+    <div className="fixed left-0 top-0 z-10 flex h-48 w-full items-center justify-between bg-white px-16 md:hidden">
+      <div className="flex items-center">
+        <FaBarsStaggered
+          className="w-24 cursor-pointer text-primary-100"
+          onClick={open}
+          style={{ strokeWidth: 15 }}
+        />
+        <p className="ml-16 text-base-semibold">{title}</p>
+      </div>
+      <FaBell className="right-0 size-24 cursor-pointer p-4 text-primary-100" />
     </div>
   );
 };
