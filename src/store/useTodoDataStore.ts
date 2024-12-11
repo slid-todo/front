e6aa589de6b4855a0e5e88aafc25415f2976128d
beom <gb3187@naver.com>
2024-12-11 12:35:10 +0900

    Refactor: 할 일 데이터 store link에 관한 네이밍 변경

--- src/store/useTodoDataStore.ts
+++ src/store/useTodoDataStore.ts
@@ -3,13 +3,13 @@ import { create } from 'zustand';
 interface TodoDataType {
   title: string;
   date: { startDate: Date | undefined; endDate: Date | undefined };
-  link?: string;
+  todoLink?: string;
   imageName?: string;
   imageEncodedBase64?: string;
   goal: { goalId: number; goalTitle: string };
   setTitle: (title: string) => void;
   setDate: (startDate: Date | undefined, endDate: Date | undefined) => void;
-  setLink: (link: string) => void;
+  setTodoLink: (todoLink: string) => void;
   setImageName: (imageName: string) => void;
   setImageEncodedBase64: (encodedFile: string) => void;
   setGoal: (goalId: number, goalTitle: string) => void;
@@ -21,21 +21,21 @@ interface TodoDataType {
 export const useTodoDataStore = create<TodoDataType>((set) => ({
   title: '',
   date: { startDate: undefined, endDate: undefined },
-  link: '',
+  todoLink: '',
   imageName: '',
   imageEncodedBase64: '',
   goal: { goalId: 0, goalTitle: '' },
   setTitle: (title: string) => set({ title: title }),
   setDate: (startDate: Date | undefined, endDate: Date | undefined) =>
     set({ date: { startDate: startDate, endDate: endDate } }),
-  setLink: (link: string) => set({ link: link }),
+  setTodoLink: (todoLink: string) => set({ todoLink: todoLink }),
   setImageName: (imageName: string) => set({ imageName: imageName }),
   setImageEncodedBase64: (encodedFile: string) =>
     set({ imageEncodedBase64: encodedFile }),
   setGoal: (goalId: number, goalTitle: string) =>
     set({ goal: { goalId: goalId, goalTitle: goalTitle } }),
   resetFile: () => set({ imageEncodedBase64: '', imageName: '' }),
-  resetLink: () => set({ link: '' }),
+  resetLink: () => set({ todoLink: '' }),
   resetAll: () =>
     set({
       title: '',
@@ -43,6 +43,6 @@ export const useTodoDataStore = create<TodoDataType>((set) => ({
       date: { startDate: undefined, endDate: undefined },
       imageEncodedBase64: '',
       imageName: '',
-      link: '',
+      todoLink: '',
     }),
 }));
