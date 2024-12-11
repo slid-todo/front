77f090080bd995064435ed2ce1e82663b20676a5
ejinn1 <ejinn1@naver.com>
2024-12-11 12:26:45 +0900

    Chore: fix typo

--- src/components/Dashboard/GoalList/GoalItem/index.tsx
+++ src/components/Dashboard/GoalList/GoalItem/index.tsx
@@ -3,12 +3,12 @@ import { ProgressLine } from '@/components/Dashboard/GoalList/GoalItem/ProgressL
 import { TodoList } from '@/components/Dashboard/GoalList/GoalItem/TodoList';
 import { TodoTypes } from '@/constants/DashboardMockData';
 
-interface GaolItemProps {
+interface GoalItemProps {
   name: string;
   todos: TodoTypes[];
 }
 
-export const GoalItem = ({ name, todos }: GaolItemProps) => {
+export const GoalItem = ({ name, todos }: GoalItemProps) => {
   return (
     <div className="relative w-full rounded-12 bg-white p-16">
       <GoalHeader title={name} />
