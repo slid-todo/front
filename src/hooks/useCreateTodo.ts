6d49a124046cf32471acfcd3f6fd7c624c9281d7
beom <gb3187@naver.com>
2024-12-11 17:22:05 +0900

    Feat: notify 적용

--- src/hooks/useCreateTodo.ts
+++ src/hooks/useCreateTodo.ts
@@ -3,6 +3,7 @@ import { AxiosError } from 'axios';
 import { createTodo } from '@/apis/createTodo';
 import { CreateTodosRequest } from '@/types/CreateTodos/CreateTodosRequest';
 import { CreateTodoResponse } from '@/types/CreateTodos/CreateTodosResponse';
+import { notify } from '@/store/useToastStore';
 
 export const useCreateTodo = (): UseMutationResult<
   CreateTodoResponse,
@@ -11,10 +12,11 @@ export const useCreateTodo = (): UseMutationResult<
 > => {
   return useMutation<CreateTodoResponse, AxiosError, CreateTodosRequest>({
     mutationFn: (data) => createTodo(data),
-    onSuccess: (data) => {
-      console.log('Todo successfully created:', data);
+    onSuccess: () => {
+      notify('success', '할 일 등록에 성공하였습니다', 2000);
     },
     onError: (error: AxiosError) => {
+      notify('error', '할 일 등록에 실패하였습니다', 2000);
       console.error('Error creating todo:', error.message);
     },
   });
