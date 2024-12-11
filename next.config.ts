557e581e1aa2ff1c08c13da1bee5ed816193c36c
dudwns0213 <dudwns0213@gmail.com>
2024-12-11 10:42:37 +0900

    Feat: next.config에 bundle-analyzer 적용

--- next.config.ts
+++ next.config.ts
@@ -1,4 +1,8 @@
-import type { NextConfig } from "next";
+/* eslint-disable @typescript-eslint/no-require-imports */
+import { NextConfig } from "next";
+const withBundleAnalyzer = require('@next/bundle-analyzer')({
+  enabled: process.env.ANALYZE === 'true',
+});
 
 const nextConfig: NextConfig = {
   /* config options here */
@@ -7,9 +11,9 @@ const nextConfig: NextConfig = {
       test: /\.svg$/,
       use: ["@svgr/webpack"],
     });
-
     return config;
   },
 };
 
-export default nextConfig;
+// withBundleAnalyzer를 통해 nextConfig를 래핑
+export default withBundleAnalyzer(nextConfig);
