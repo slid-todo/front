5e799e8d3fd3879cee10fee818c040ee0b3759f6
ejinn1 <ejinn1@naver.com>
2024-12-11 13:55:39 +0900

    Feat: add Card component

--- src/app/(route)/dashboard/page.tsx
+++ src/app/(route)/dashboard/page.tsx
@@ -1,7 +1,10 @@
 import { FaAngleRight } from 'react-icons/fa6';
+
 import { Header } from '@/components/common/Header';
 import { GoalList } from '@/components/Dashboard/GoalList';
 import { MyProgress } from '@/components/Dashboard/MyProgress';
+
+import { Card } from '@/components/common/Card';
 import { GOALS } from '@/constants/DashboardMockData';
 
 export default function DashBoardPage() {
@@ -9,8 +12,17 @@ export default function DashBoardPage() {
     <>
       <Header title="대시보드" />
       <div className="flex min-h-screen w-screen flex-col gap-16 overflow-y-scroll bg-custom-white-100 px-16 pb-16 pt-48">
+        {/* 팔로워 */}
+        <div className="mt-16 w-full">
+          <Card>
+            <p className="text-sm-normal text-custom-gray-100">
+              찍찍이들 팔로우 하고 인증 보기
+            </p>
+          </Card>
+        </div>
+
         {/* 최근 등록 */}
-        <div className="relative mt-16 w-full rounded-12 bg-white p-16">
+        <div className="relative w-full rounded-12 bg-white p-16">
           <p className="text-lg-semibold">최근 등록한 할 일</p>
           <button className="flex-center absolute right-16 top-16 text-sm-medium">
             모두 보기 <FaAngleRight className="ml-8" />
@@ -28,7 +40,7 @@ export default function DashBoardPage() {
           </ul>
         </div>
 
-        <MyProgress progressPercent={74} />
+        <MyProgress progressPercent={0} />
         <GoalList goals={GOALS} />
       </div>
     </>
