e9bd8d957563a23e7b77a7c4dec790b6d26723d5
beom <gb3187@naver.com>
2024-12-13 23:17:21 +0900

    Refactor: axios 인스턴스 인터셉터 설정

--- src/apis/Auth/signin.ts
+++ src/apis/Auth/signin.ts
@@ -1,16 +1,16 @@
-import axios from 'axios';
 import { API_ENDPOINTS } from '@/constants/ApiEndpoints';
 import { AuthDataRequest } from '@/types/Auth/AuthDataRequest';
+import axiosInstance from '../../../lib/axiosInstance';
 
 export const signin = async (data: AuthDataRequest) => {
   const baseurl = process.env.NEXT_PUBLIC_API_URL;
 
   try {
-    const response = await axios.post(
+    const response = await axiosInstance.post(
       baseurl + API_ENDPOINTS.AUTH.SIGN_IN,
       data,
     );
-    return response.data;
+    return response;
   } catch (error) {
     console.error('Error signin:', error);
     throw error;
--- src/lib/axiosInstance.ts
+++ src/lib/axiosInstance.ts
@@ -2,33 +2,27 @@ import axios from 'axios';
 
 const apiUrl = process.env.NEXT_PUBLIC_API_URL;
 
-const token = process.env.NEXT_PUBLIC_TEST_TOKEN;
-
 const axiosInstance = axios.create({
   baseURL: apiUrl,
   headers: {
     'Content-Type': 'application/json',
     Accept: '*/*',
-    token: token, // 여기서 기본 헤더를 설정
   },
 });
 
-// // 요청 인터셉터를 사용하여 토큰을 동적으로 헤더에 추가합니다.
-// axiosInstance.interceptors.request.use(
-//   (config) => {
-//     // 로컬 스토리지에서 토큰을 가져옵니다.
-//     const token = localStorage.getItem('token'); // 또는 쿠키에서 가져올 수도 있음
+axiosInstance.interceptors.request.use(
+  (config) => {
+    const token = localStorage.getItem('token');
 
-//     // 토큰이 있을 경우 Authorization 헤더에 추가
-//     if (token) {
-//       config.headers['Authorization'] = `Bearer ${token}`;
-//     }
+    if (token) {
+      config.headers['token'] = token;
+    }
 
-//     return config;
-//   },
-//   (error) => {
-//     return Promise.reject(error);
-//   },
-// );
+    return config;
+  },
+  (error) => {
+    return Promise.reject(error);
+  },
+);
 
 export default axiosInstance;
