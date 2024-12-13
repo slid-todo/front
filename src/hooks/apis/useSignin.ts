e29cea0812fde02984953285b2b6308926ee251f
beom <gb3187@naver.com>
2024-12-13 23:16:22 +0900

    Feat: 로그인 성공시 토큰 local storage 저장

--- src/hooks/apis/useSignin.ts
+++ src/hooks/apis/useSignin.ts
@@ -1,20 +1,19 @@
 import { useMutation, UseMutationResult } from '@tanstack/react-query';
-import { AxiosError } from 'axios';
+import { AxiosError, AxiosResponse } from 'axios';
 import { signin } from '@/apis/Auth/signin';
 import { notify } from '@/store/useToastStore';
-import { AuthDataResponse } from '@/types/Auth/AuthDataResponse';
 import { AuthDataRequest } from '@/types/Auth/AuthDataRequest';
 
 export const useSignin = (): UseMutationResult<
-  AuthDataResponse,
+  AxiosResponse,
   AxiosError,
   AuthDataRequest
 > => {
   return useMutation({
     mutationFn: (data) => signin(data),
-    onSuccess: (data) => {
+    onSuccess: (response) => {
       notify('success', '로그인에 성공하였습니다', 2000);
-      console.log(data); //응답 값 확인을 위한
+      localStorage.setItem('token', response.headers.token);
     },
     onError: (error: AxiosError) => {
       notify('error', '로그인에 실패하였습니다', 2000);
