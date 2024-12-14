1b2eca4ecc3b0c1dfa177f80a86c7a552623f695
beom <gb3187@naver.com>
2024-12-14 22:45:00 +0900

    Feat: 비밀번호 확인에 관한 타입 변경

--- src/constants/Placeholders.ts
+++ src/constants/Placeholders.ts
@@ -7,5 +7,5 @@ export const PLACEHOLDERS = {
   NAME: '이름을 입력해주세요',
   EMAIL: '이메일을 입력해주세요',
   PASSWORD: '비밀번호를 입력해주세요',
-  PASSWORDCHK: '비밀번호를 다시 입력해주세요',
+  PASSWORD_CHECK: '비밀번호를 다시 입력해주세요',
 };
