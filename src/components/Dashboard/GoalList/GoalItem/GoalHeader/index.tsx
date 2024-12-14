43a0869f17f7a3290a3c52e5d5d582893cfbb293
ejinn1 <ejinn1@naver.com>
2024-12-15 03:17:26 +0900

    Refactor: add link todos and add todo modal

--- src/components/Dashboard/GoalList/GoalItem/GoalHeader/index.tsx
+++ src/components/Dashboard/GoalList/GoalItem/GoalHeader/index.tsx
@@ -1,20 +1,29 @@
 import { FaFlag, FaPlus } from 'react-icons/fa6';
 
+import TodoModal from '@/components/TodoModal/TodoModalContainer';
+import { useTodoModalStore } from '@/store/useTodoModalStore';
+
 interface GoalHeaderProps {
   title: string;
 }
 
 export const GoalHeader = ({ title }: GoalHeaderProps) => {
+  const { isOpen, open } = useTodoModalStore();
+
   return (
     <>
       <span className="inline-flex items-center text-base-semibold">
         <FaFlag className="mr-4 size-24 p-2 text-sub-purple" />
         {title}
       </span>
-      <button className="absolute right-16 top-16 flex items-center text-primary-100">
+      <button
+        onClick={open}
+        className="absolute right-16 top-16 flex items-center text-primary-100"
+      >
         <FaPlus className="size-24 p-4" />
         <span className="text-sm-semibold">할일 추가</span>
       </button>
+      {isOpen && <TodoModal todoType="생성" />}
     </>
   );
 };
