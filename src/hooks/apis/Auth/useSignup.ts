9f2bc310fd9e0a76d0878f7575755b5835b1a446
beom <gb3187@naver.com>
2024-12-14 22:46:31 +0900

    Feat: api 함수 생성 및 등록 mutation 커스텀 훅

---
+++ src/hooks/apis/Auth/useSignup.ts
@@ -0,0 +1,26 @@
+import { useMutation, UseMutationResult } from '@tanstack/react-query';
+import { useRouter } from 'next/navigation';
+import { AxiosError, AxiosResponse } from 'axios';
+import { signup } from '@/apis/Auth/signup';
+import { notify } from '@/store/useToastStore';
+import { AuthDataRequest } from '@/types/Auth/AuthDataRequest';
+
+export const useSignup = (): UseMutationResult<
+  AxiosResponse,
+  AxiosError,
+  AuthDataRequest
+> => {
+  const router = useRouter();
+
+  return useMutation({
+    mutationFn: (data) => signup(data),
+    onSuccess: () => {
+      notify('success', '회원가입에 성공하였습니다', 2000);
+      router.push('/signin');
+    },
+    onError: (error: AxiosError) => {
+      notify('error', '회원가입에 실패하였습니다', 2000);
+      console.error('Error Sign up:', error.message);
+    },
+  });
+};
