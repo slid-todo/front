198145c61de2a14a163c189cfa66faa5f3945bd7
beom <gb3187@naver.com>
2024-12-14 15:52:55 +0900

    Refactor: 로그인 api 함수 불필요한 문장

--- src/apis/Auth/signin.ts
+++ src/apis/Auth/signin.ts
@@ -3,13 +3,8 @@ import { AuthDataRequest } from '@/types/Auth/AuthDataRequest';
 import axiosInstance from '../../../lib/axiosInstance';
 
 export const signin = async (data: AuthDataRequest) => {
-  const baseurl = process.env.NEXT_PUBLIC_API_URL;
-
   try {
-    const response = await axiosInstance.post(
-      baseurl + API_ENDPOINTS.AUTH.SIGN_IN,
-      data,
-    );
+    const response = await axiosInstance.post(API_ENDPOINTS.AUTH.SIGN_IN, data);
     return response;
   } catch (error) {
     console.error('Error signin:', error);
