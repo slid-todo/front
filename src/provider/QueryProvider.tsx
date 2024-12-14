f57a828366d430a797a934ff0e09b57657365dcb
ejinn1 <ejinn1@naver.com>
2024-12-14 16:07:59 +0900

    Rename: move lib folder in src

--- src/provider/QueryProvider.tsx
+++ src/provider/QueryProvider.tsx
@@ -4,7 +4,7 @@ import { QueryClientProvider } from '@tanstack/react-query';
 import { ReactQueryDevtools } from '@tanstack/react-query-devtools';
 
 import { Toast } from '@/components/common/Toast';
-import { getQueryClient } from '@/utils/query/getQueryClient';
+import { getQueryClient } from '@/lib/query/getQueryClient';
 
 interface QueryProviderProps {
   children: React.ReactNode;
