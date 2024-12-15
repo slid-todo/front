281077957c86292a4e19cca85d88c37a0b2ccc98
YoungJun Bae <dudwns0213@gmail.com>
2024-12-15 14:26:10 +0900

    Docs: camera 각주 추가(추후 구현)

--- src/components/Camera/index.tsx
+++ src/components/Camera/index.tsx
@@ -1,8 +1,10 @@
-import React, { useRef } from 'react';
+import { useRef } from 'react';
 import Webcam from 'react-webcam';
 
 const Camera = () => {
   const webcamRef = useRef<Webcam>(null);
+
+  //해당 파일은 현재 구현만 한 상태이고, 추후 17일 이후 데스크탑 직접적으로 구현할 때 사용할 예정입니다.
 
   const capture = () => {
     if (webcamRef.current) {
@@ -13,7 +15,7 @@ const Camera = () => {
 
   return (
     <div className="flex flex-col items-center space-y-4">
-      <h2 className="text-xl font-semibold">React Webcam으로 카메라 촬영</h2>
+      <h2 className="text-xl font-semibold">React Webcam</h2>
       <Webcam
         audio={false}
         ref={webcamRef}
