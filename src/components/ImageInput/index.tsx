61c6398d4e0352f45a60cdd7503f726f01107f30
YoungJun Bae <dudwns0213@gmail.com>
2024-12-15 20:08:50 +0900

    Chore: InputModalContent re-export

--- src/components/ImageInput/index.tsx
+++ src/components/ImageInput/index.tsx
@@ -1,5 +1,6 @@
 import { DesktopCapture } from './DesktopCapture';
 import { MobileCapture } from './MobileCapture';
 import { GalleryPicker } from './GalleryPicker';
+import { InputModalContent } from './InputModalContent';
 
-export { DesktopCapture, MobileCapture, GalleryPicker };
+export { DesktopCapture, MobileCapture, GalleryPicker, InputModalContent };
