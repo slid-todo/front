0efbb38531904b50083f30d487478017d200c945
beom <gb3187@naver.com>
2024-12-15 12:14:32 +0900

    Feat: 임시 로고 설정(아이콘 대체)

--- src/components/AuthPage/Logo/index.tsx
+++ src/components/AuthPage/Logo/index.tsx
@@ -1,10 +1,10 @@
-import { MdVisibilityOff } from 'react-icons/md';
+import { GiSeatedMouse } from 'react-icons/gi';
 import Link from 'next/link';
 
 export const Logo = () => {
   return (
     <Link href={'/'} className="flex-center h-89 w-270 px-12 py-20 ">
-      <MdVisibilityOff className="size-150 cursor-pointer" />
+      <GiSeatedMouse className="size-150 cursor-pointer" />
     </Link>
   );
 };
