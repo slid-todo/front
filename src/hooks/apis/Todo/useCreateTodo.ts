ae9fa4b7de71520c08ee75366b217501c72441bb
beom <gb3187@naver.com>
2024-12-15 21:28:57 +0900

    Refactor: 할일 생성 성공 시 쿼리키 무효화 후 모달 닫기

--- src/hooks/apis/Todo/useCreateTodo.ts
+++ src/hooks/apis/Todo/useCreateTodo.ts
@@ -1,19 +1,31 @@
-import { useMutation, UseMutationResult } from '@tanstack/react-query';
+import {
+  useMutation,
+  UseMutationResult,
+  useQueryClient,
+} from '@tanstack/react-query';
 import { AxiosError } from 'axios';
 import { createTodo } from '@/apis/Todo/createTodo';
 import { CreateTodosRequest } from '@/types/CreateTodos/CreateTodosRequest';
 import { CreateTodoResponse } from '@/types/CreateTodos/CreateTodosResponse';
 import { notify } from '@/store/useToastStore';
+import { QUERY_KEYS } from '@/constants/QueryKeys';
+import { useTodoModalStore } from '@/store/useTodoModalStore';
 
 export const useCreateTodo = (): UseMutationResult<
   CreateTodoResponse,
   AxiosError,
   CreateTodosRequest
 > => {
+  const queryClient = useQueryClient();
+  const { close } = useTodoModalStore();
+
   return useMutation<CreateTodoResponse, AxiosError, CreateTodosRequest>({
     mutationFn: (data) => createTodo(data),
     onSuccess: () => {
       notify('success', '할 일 등록에 성공하였습니다', 2000);
+      queryClient.invalidateQueries({ queryKey: [QUERY_KEYS.TODOS_OF_GOALS] });
+      queryClient.invalidateQueries({ queryKey: [QUERY_KEYS.RECENT_TODOS] });
+      close();
     },
     onError: (error: AxiosError) => {
       notify('error', '할 일 등록에 실패하였습니다', 2000);
