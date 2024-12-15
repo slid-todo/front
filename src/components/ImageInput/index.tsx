c5794004de30e1348bed42adfff5705c3fd325f2
YoungJun Bae <dudwns0213@gmail.com>
2024-12-15 17:43:10 +0900

    Chore: ImageInput re-export

---
+++ src/components/ImageInput/index.tsx
@@ -0,0 +1,5 @@
+import { DesktopCapture } from './DesktopCapture';
+import { MobileCapture } from './MobileCapture';
+import { GalleryPicker } from './GalleryPicker';
+
+export { DesktopCapture, MobileCapture, GalleryPicker };
