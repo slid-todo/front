f0e6a01cfc5745a454844633a0b893e6922ba585
ejinn1 <ejinn1@naver.com>
2024-12-16 13:51:24 +0900

    Rename: change name get goals

--- src/constants/ApiEndpoints.ts
+++ src/constants/ApiEndpoints.ts
@@ -11,6 +11,7 @@ export const API_ENDPOINTS = {
   AUTH: {
     SIGN_IN: '/api/v1/auths/login',
     SIGN_UP: '/api/v1/auths/signup',
+    USER: '/api/v1/auths/user',
   },
   GOAL: {
     GOALS: '/api/v1/goals',
--- src/constants/QueryKeys.ts
+++ src/constants/QueryKeys.ts
@@ -3,4 +3,5 @@ export const QUERY_KEYS = {
   TODOS_OF_GOALS: 'todosOfGoals',
   TODAY_PROGRESS: 'todayProgress',
   RECENT_TODOS: 'recentTodos',
+  USER_INFO: 'userInfo',
 };
