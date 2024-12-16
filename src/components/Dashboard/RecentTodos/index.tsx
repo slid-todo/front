a935148154a111de750dbf54453095a4b8e4bf96
ejinn1 <ejinn1@naver.com>
2024-12-16 18:18:07 +0900

    Feat: add recent todos skeleton

--- src/components/Dashboard/RecentTodos/index.tsx
+++ src/components/Dashboard/RecentTodos/index.tsx
@@ -8,10 +8,11 @@ import { DashboardItemContainer } from '@/components/Dashboard/DashboardItemCont
 import { TodoItem } from '@/components/Todos';
 import { Button } from '@/components/common/Button/Button';
 import { Card } from '@/components/common/Card';
+import { Skeleton } from '@/components/common/Skeleton';
 import { useRecentTodosQuery } from '@/hooks/apis/Dashboard/useRecnetTodosQuery';
 
 export const RecentTodos = () => {
-  const { todos } = useRecentTodosQuery();
+  const { todos, isLoading } = useRecentTodosQuery();
 
   return (
     <DashboardItemContainer title="최근 등록한 할일" className="relative">
@@ -21,7 +22,19 @@ export const RecentTodos = () => {
       >
         모두 보기 <FaAngleRight className="ml-8" />
       </Link>
-      {todos.length === 0 ? (
+      {isLoading ? (
+        <div>
+          {Array.from({ length: 3 }, (_, index) => (
+            <div key={index} className="flex h-72">
+              <Skeleton className="my-8 size-56 rounded-16" />
+              <div className="ml-16 flex h-72 flex-col items-start justify-center gap-10">
+                <Skeleton className="h-12 w-40 rounded-16" />
+                <Skeleton className="h-20 w-120 rounded-16" />
+              </div>
+            </div>
+          ))}
+        </div>
+      ) : todos.length === 0 ? (
         <Card>
           <p className="text-sm-normal text-custom-gray-100">
             목표 먼저 등록 후 할 일을 설정해주세요.
