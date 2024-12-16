3634a81e48f60ebe6c169a2b251bc33c9eb36ee6
ejinn1 <ejinn1@naver.com>
2024-12-16 19:38:24 +0900

    Refactor: add sidebar router push

--- src/components/Sidebar/MenuItem/index.tsx
+++ src/components/Sidebar/MenuItem/index.tsx
@@ -4,11 +4,20 @@ interface MenuItemProps {
   icon: ReactNode;
   label: string;
   addButton?: ReactNode;
+  onClick?: () => void;
 }
 
-export const MenuItem = ({ icon, label, addButton = null }: MenuItemProps) => {
+export const MenuItem = ({
+  icon,
+  label,
+  addButton = null,
+  onClick,
+}: MenuItemProps) => {
   return (
-    <div className="flex w-full items-center justify-between border-t border-slate-200 p-16 sm:block">
+    <div
+      onClick={onClick}
+      className="flex w-full items-center justify-between border-t border-slate-200 p-16 sm:block"
+    >
       <div className="flex items-center gap-8 md:pb-16">
         {icon}
         <span className="text-xl-semibold">{label}</span>
