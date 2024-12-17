1cfe423d234aff6ff9150b966b65331f906c2063
beom <gb3187@naver.com>
2024-12-17 17:55:27 +0900

    Refactor: 할 일 생성 성공 시 데이터 초기화

--- src/hooks/apis/Todo/useCreateTodo.ts
+++ src/hooks/apis/Todo/useCreateTodo.ts
@@ -10,6 +10,7 @@ import { CreateTodoResponse } from '@/types/CreateTodos/CreateTodosResponse';
 import { notify } from '@/store/useToastStore';
 import { QUERY_KEYS } from '@/constants/QueryKeys';
 import { useTodoModalStore } from '@/store/useTodoModalStore';
+import { useTodoDataStore } from '@/store/useTodoDataStore';
 
 export const useCreateTodo = (): UseMutationResult<
   CreateTodoResponse,
@@ -18,6 +19,7 @@ export const useCreateTodo = (): UseMutationResult<
 > => {
   const queryClient = useQueryClient();
   const { close } = useTodoModalStore();
+  const { resetAll } = useTodoDataStore();
 
   return useMutation<CreateTodoResponse, AxiosError, CreateTodosRequest>({
     mutationFn: (data) => createTodo(data),
@@ -25,6 +27,7 @@ export const useCreateTodo = (): UseMutationResult<
       notify('success', '등록에 성공하였습니다', 3000);
       queryClient.invalidateQueries({ queryKey: [QUERY_KEYS.TODOS_OF_GOALS] });
       queryClient.invalidateQueries({ queryKey: [QUERY_KEYS.RECENT_TODOS] });
+      resetAll();
       close();
     },
     onError: (error: AxiosError) => {
