ac982a85004b6fa6ebd5a6547385df64b08aae89
beom <gb3187@naver.com>
2024-12-17 16:47:50 +0900

    Feat: 할 일 수정 관련 엔드포인트 추가

--- src/constants/ApiEndpoints.ts
+++ src/constants/ApiEndpoints.ts
@@ -2,6 +2,7 @@ export const API_ENDPOINTS = {
   TODOS: {
     GET_ALL: '/api/v1/todos',
     CREATE: '/api/v1/todos',
+    PUT: (todoId: string | number) => `api/vi/todos/${todoId}`,
     GET_GOALS: '/api/v1/todos/goals',
     GET_GOAL_BY_ID: (goalId: string | number) =>
       `/api/v1/todos/goals/${goalId}`,
