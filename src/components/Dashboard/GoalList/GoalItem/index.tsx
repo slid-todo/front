38b882a0cba8318fd3d71d4b3ecefbeb1875be5f
beom <gb3187@naver.com>
2024-12-17 17:09:40 +0900

    Refactor: 할 일 목표에 id값 전달 props 추가

--- src/components/Dashboard/GoalList/GoalItem/index.tsx
+++ src/components/Dashboard/GoalList/GoalItem/index.tsx
@@ -7,15 +7,16 @@ import { TodoList } from '@/components/Dashboard/GoalList/GoalItem/TodoList';
 import { TodosResponse } from '@/hooks/apis/Dashboard/useTodosOfGoalsQuery';
 
 interface GoalItemProps {
+  id: number;
   title: string;
   color: string;
   todos: TodosResponse[];
 }
 
-export const GoalItem = ({ title, color, todos }: GoalItemProps) => {
+export const GoalItem = ({ id, title, color, todos }: GoalItemProps) => {
   return (
     <div className="relative w-full rounded-12 bg-white p-16 shadow-sm">
-      <GoalHeader title={title} color={color} />
+      <GoalHeader id={id} title={title} color={color} />
       <ProgressLine percent={0} color={color} />
       {todos.map((todo) => (
         <TodoList key={todo.todoId} todo={todo} color={color} />
