22a5c3913c99ec7f01d2933e705649a7f5281474
beom <gb3187@naver.com>
2024-12-17 16:47:07 +0900

    Feat: 할 일 수정 api 구현

---
+++ src/apis/Todo/modifyTodo.ts
@@ -0,0 +1,19 @@
+import { API_ENDPOINTS } from '@/constants/ApiEndpoints';
+import { ModifyTodosRequest } from '@/types/ModifyTodos/ModifyTodosRequest';
+import axiosInstance from '@/lib/axiosInstance';
+
+export const modifyTodo = async (
+  todoId: string | number,
+  data: ModifyTodosRequest,
+) => {
+  try {
+    const response = await axiosInstance.put(
+      API_ENDPOINTS.TODOS.PUT(todoId),
+      data,
+    );
+    return response.data;
+  } catch (error) {
+    console.error('Error modifying todo: ', error);
+    throw error;
+  }
+};
