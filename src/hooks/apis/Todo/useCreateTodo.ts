f35d2acefe934547c8abf64c4255c2756c2c4643
ejinn1 <ejinn1@naver.com>
2024-12-18 16:38:54 +0900

    Refactor: change logic add todos

--- src/hooks/apis/Todo/useCreateTodo.ts
+++ src/hooks/apis/Todo/useCreateTodo.ts
@@ -4,13 +4,15 @@ import {
   useQueryClient,
 } from '@tanstack/react-query';
 import { AxiosError } from 'axios';
+
 import { createTodo } from '@/apis/Todo/createTodo';
+import { QUERY_KEYS } from '@/constants/QueryKeys';
+import { useSidebarStore } from '@/store/useSidebarStore';
+import { notify } from '@/store/useToastStore';
+import { useTodoDataStore } from '@/store/useTodoDataStore';
+import { useTodoModalStore } from '@/store/useTodoModalStore';
 import { CreateTodosRequest } from '@/types/CreateTodos/CreateTodosRequest';
 import { CreateTodoResponse } from '@/types/CreateTodos/CreateTodosResponse';
-import { notify } from '@/store/useToastStore';
-import { QUERY_KEYS } from '@/constants/QueryKeys';
-import { useTodoModalStore } from '@/store/useTodoModalStore';
-import { useTodoDataStore } from '@/store/useTodoDataStore';
 
 export const useCreateTodo = (): UseMutationResult<
   CreateTodoResponse,
@@ -20,6 +22,7 @@ export const useCreateTodo = (): UseMutationResult<
   const queryClient = useQueryClient();
   const { close } = useTodoModalStore();
   const { resetAll } = useTodoDataStore();
+  const { close: closeSidebar } = useSidebarStore();
 
   return useMutation<CreateTodoResponse, AxiosError, CreateTodosRequest>({
     mutationFn: (data) => createTodo(data),
@@ -29,6 +32,7 @@ export const useCreateTodo = (): UseMutationResult<
       queryClient.invalidateQueries({ queryKey: [QUERY_KEYS.RECENT_TODOS] });
       resetAll();
       close();
+      closeSidebar();
     },
     onError: (error: AxiosError) => {
       notify('error', '등록에 실패하였습니다', 3000);
