e8a887c5228beee1cd5ff15d3f1c9f3300ac7e69
beom <gb3187@naver.com>
2024-12-18 16:34:27 +0900

    Feat: 할일 생성, 수정을 위한 데이터 재구성 및 분리

---
+++ src/constants/Todos/CommonTodoData.ts
@@ -0,0 +1,11 @@
+export const COMMON_TODO_DATA = {
+  title: '',
+  startDate: '',
+  endDate: '',
+  todoLink: '',
+  todoStatus: '',
+  imageName: '',
+  imageEncodedBase64: '',
+  goalId: 0,
+  goalTitle: '',
+};
