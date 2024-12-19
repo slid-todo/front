c80b50091c5ddb53fdeb4dae5c2e62623b8dc88c
beom <gb3187@naver.com>
2024-12-19 14:23:59 +0900

    Refactor: 이미지 미리 보기 구현 및 업로드 버튼 수정

--- src/constants/Placeholders.ts
+++ src/constants/Placeholders.ts
@@ -2,7 +2,7 @@ export const PLACEHOLDERS = {
   TARGET: '목표를 선택해주세요',
   TITLE_TODO: '할 일의 제목을 적어주세요',
   FILE: '파일을 업로드해주세요',
-  LINK_INPUT: '영상이나 글, 파일의 링크를 넣어주세요',
+  LINK_INPUT: '링크를 첨부해주세요',
   DATE_RANGE: '날짜 범위를 선택해주세요',
   NAME: '이름을 입력해주세요',
   EMAIL: '이메일을 입력해주세요',
