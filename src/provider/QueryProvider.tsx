8988e6154bafa071eb08ff811b5818b92a141a15
beom <gb3187@naver.com>
2024-12-19 09:17:00 +0900

    Refactor: provider에 todoModal 추가

--- src/provider/QueryProvider.tsx
+++ src/provider/QueryProvider.tsx
@@ -6,7 +6,6 @@ import { ReactQueryDevtools } from '@tanstack/react-query-devtools';
 import { Toast } from '@/components/common/Toast';
 import TodoModal from '@/components/TodoModal/TodoModalContainer';
 import { getQueryClient } from '@/lib/query/getQueryClient';
-import { useTodoModalStore } from '@/store/useTodoModalStore';
 
 interface QueryProviderProps {
   children: React.ReactNode;
@@ -15,14 +14,12 @@ interface QueryProviderProps {
 const QueryProvider: React.FC<QueryProviderProps> = ({ children }) => {
   const queryClient = getQueryClient();
 
-  const { isOpen } = useTodoModalStore();
-
   return (
     <QueryClientProvider client={queryClient}>
       {children}
       <ReactQueryDevtools initialIsOpen={true} />
       <Toast />
-      {isOpen && <TodoModal todoType="생성" />}
+      <TodoModal />
     </QueryClientProvider>
   );
 };
