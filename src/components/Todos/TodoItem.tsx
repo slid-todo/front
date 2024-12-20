a06669bc1f07f3dc663afd287b39bf051e0645ee
dudwns0213 <dudwns0213@gmail.com>
2024-12-21 02:43:32 +0900

    Feat: 인증 안 된 Todo onClick 이벤트 추가(InputModalContent)

--- src/components/Todos/TodoItem.tsx
+++ src/components/Todos/TodoItem.tsx
@@ -1,27 +1,65 @@
-import { FaCamera } from 'react-icons/fa6';
+// components/Todos/TodoItem.tsx
 
-interface TodoItemProps {
-  title: string;
-  goal: string;
+'use client';
+
+import { useState } from 'react';
+import { FaCamera } from 'react-icons/fa6';
+import Image from 'next/image';
+import { TodayTodoItem } from '@/hooks/apis/Todo/useTodayTodo';
+import { InputModalContent } from '../ImageInput';
+
+interface TodoItemProps extends TodayTodoItem {
   className?: string;
-  color?: string;
 }
 
 export const TodoItem = (props: TodoItemProps) => {
-  const { title, goal, color, className = '' } = props;
+  const { todoTitle, complete, className = '' } = props;
+
+  const [isGalleryModalOpen, setIsGalleryModalOpen] = useState(false);
+
+  const handleOpenGalleryModal = () => {
+    setIsGalleryModalOpen(true);
+  };
+
+  const handleCloseGalleryModal = () => {
+    setIsGalleryModalOpen(false);
+  };
 
   return (
-    <div className={`flex h-72 ${className}`}>
-      <div
-        className="flex-center my-8 size-56 rounded-16 bg-sub-purple"
-        style={{ backgroundColor: color }}
-      >
-        <FaCamera fill="white" />
+    <>
+      <div className={`flex h-72 ${className}`}>
+        {complete.completePic ? (
+          <div className="flex-center my-8 size-56 overflow-hidden rounded-16 bg-sub-purple">
+            <Image
+              src={complete.completePic}
+              alt="Complete Picture"
+              width={56}
+              height={56}
+              className="object-cover"
+            />
+          </div>
+        ) : (
+          <div
+            className="flex-center my-8 size-56 cursor-pointer rounded-16 bg-sub-purple"
+            onClick={handleOpenGalleryModal}
+          >
+            <FaCamera fill="white" />
+          </div>
+        )}
+        <div className="my-14 ml-16">
+          <div className="text-xs-medium text-custom-gray-100">
+            현재 목표가 안 나와요 ㅠ
+          </div>
+          <div className="text-base-medium text-custom-gray-300">
+            {todoTitle}
+          </div>
+        </div>
       </div>
-      <div className="my-14 ml-16">
-        <div className="text-xs-medium text-custom-gray-100">{goal}</div>
-        <div className="text-base-medium text-custom-gray-300">{title}</div>
-      </div>
-    </div>
+
+      <InputModalContent
+        isOpen={isGalleryModalOpen}
+        onClose={handleCloseGalleryModal}
+      />
+    </>
   );
 };
