c6bafc0c822a192e94d94277a307ee71a35cf761
dudwns0213 <dudwns0213@gmail.com>
2024-12-21 03:01:42 +0900

    Style: Modal Style 적용

--- src/components/CertifiedModal/index.tsx
+++ src/components/CertifiedModal/index.tsx
@@ -1,6 +1,8 @@
 'use client';
 
-import { ModalContent } from '../common/Modal';
+import { motion } from 'motion/react';
+import { todoModalVariants } from '@/constants/motionVariants';
+import { ModalContainer } from '../common/Modal';
 import { VerificationNote } from '../VerificationNote/VerificationNoteContainer';
 
 interface CertifiedModalProps {
@@ -11,9 +13,24 @@ interface CertifiedModalProps {
 export const CertifiedModal = (props: CertifiedModalProps) => {
   const { isOpen, onClose } = props;
 
+  const handleClose = () => {
+    onClose();
+  };
+
+  if (!isOpen) {
+    return null;
+  }
+
   return (
-    <ModalContent isOpen={isOpen} onClose={onClose}>
-      <VerificationNote />
-    </ModalContent>
+    <ModalContainer onClose={handleClose}>
+      <motion.div
+        variants={todoModalVariants}
+        initial="hidden"
+        animate="visible"
+        className="flex size-full flex-col items-start gap-10 overflow-y-auto bg-custom-white-100 px-16 py-24 sm:h-auto sm:w-520 sm:rounded-12 sm:p-24"
+      >
+        <VerificationNote />
+      </motion.div>
+    </ModalContainer>
   );
 };
