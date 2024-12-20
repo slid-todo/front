11301174f28a4c26ff4b1296aa2cc7a3b22fab37
dudwns0213 <dudwns0213@gmail.com>
2024-12-21 01:39:42 +0900

    Feat: 진행, 완료 상태에 따라 2개의 Todos를 return하도록 변경

--- src/hooks/useFilteredTodos.ts
+++ src/hooks/useFilteredTodos.ts
@@ -1,18 +1,28 @@
 import { useMemo } from 'react';
-import { TodayTodoItem } from './apis/Todo/useTodayTodo';
+import { TodayTodoItem } from '@/hooks/apis/Todo/useTodayTodo';
 
 export function useFilteredTodos(
   todos: TodayTodoItem[],
   currentSortFilter: string,
 ) {
-  const filteredTodos = useMemo(() => {
-    if (currentSortFilter === '진행') {
-      return todos.filter((todo) => todo.complete.completeStatus === '진행');
-    } else if (currentSortFilter === '완료') {
-      return todos.filter((todo) => todo.complete.completeStatus === '완료');
+  const { inProgressTodos, completedTodos } = useMemo(() => {
+    let inProgress: TodayTodoItem[] = [];
+    let completed: TodayTodoItem[] = [];
+
+    if (currentSortFilter === '진행' || currentSortFilter === '전체') {
+      inProgress = todos.filter(
+        (todo) => todo.complete.completeStatus === '진행',
+      );
     }
-    return todos;
+
+    if (currentSortFilter === '완료' || currentSortFilter === '전체') {
+      completed = todos.filter(
+        (todo) => todo.complete.completeStatus === '완료',
+      );
+    }
+
+    return { inProgressTodos: inProgress, completedTodos: completed };
   }, [todos, currentSortFilter]);
 
-  return { filteredTodos };
+  return { inProgressTodos, completedTodos };
 }
