4cdb0c746c04c2644862ee88e9b865936a4cdfe6
ejinn1 <ejinn1@naver.com>
2024-12-22 00:08:11 +0900

    Refactor: change token handling cookie to header

--- src/apis/Auth/signin.ts
+++ src/apis/Auth/signin.ts
@@ -1,10 +1,15 @@
-import { API_ENDPOINTS } from '@/constants/ApiEndpoints';
 import axiosInstance from '@/lib/axiosInstance';
-import { AuthDataRequest } from '@/types/Auth/AuthDataRequest';
+
+import { API_ENDPOINTS } from '@/constants/ApiEndpoints';
+import { useTokenStore } from '@/store/useTokenStore';
+import type { AuthDataRequest } from '@/types/Auth/AuthDataRequest';
 
 export const signin = async (data: AuthDataRequest) => {
   try {
     const response = await axiosInstance.post(API_ENDPOINTS.AUTH.SIGN_IN, data);
+
+    useTokenStore.getState().setToken(response.headers.token || '');
+
     return response;
   } catch (error) {
     console.error('Error signin:', error);
