1651ae9687431cd07504d4b102fc25f6c2368249
beom <gb3187@naver.com>
2024-12-22 16:25:49 +0900

    Refactor: 토큰 저장 방식을 쿠키로 설정 (미들웨어, 로그인, axiosInstance)

--- src/apis/Auth/signin.ts
+++ src/apis/Auth/signin.ts
@@ -1,15 +1,11 @@
 import axiosInstance from '@/lib/axiosInstance';
 
 import { API_ENDPOINTS } from '@/constants/ApiEndpoints';
-import { useTokenStore } from '@/store/useTokenStore';
 import type { AuthDataRequest } from '@/types/Auth/AuthDataRequest';
 
 export const signin = async (data: AuthDataRequest) => {
   try {
     const response = await axiosInstance.post(API_ENDPOINTS.AUTH.SIGN_IN, data);
-
-    useTokenStore.getState().setToken(response.headers.token || '');
-
     return response;
   } catch (error) {
     console.error('Error signin:', error);
