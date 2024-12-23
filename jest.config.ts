0eaa8e522e6dff27d8dcfacce369028ab329473c
ejinn1 <ejinn1@naver.com>
2024-12-23 17:36:06 +0900

    Setting: add jest test

---
+++ jest.config.ts
@@ -0,0 +1,20 @@
+import type { Config } from 'jest'
+import nextJest from 'next/jest.js'
+
+const createJestConfig = nextJest({
+  dir: './',
+})
+
+// Add any custom config to be passed to Jest
+const config: Config = {
+  coverageProvider: 'v8',
+  testEnvironment: 'node',
+  testMatch: [
+    '**/__test__/**/*.(ts|tsx)',
+  ],
+  // Add more setup options before each test is run
+  // setupFilesAfterEnv: ['<rootDir>/jest.setup.ts'],
+}
+
+// createJestConfig is exported this way to ensure that next/jest can load the Next.js config which is async
+export default createJestConfig(config)
