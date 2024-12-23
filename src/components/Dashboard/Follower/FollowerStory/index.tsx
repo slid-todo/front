b6c543dec438947a5ef623a30628e95b9709617d
ejinn1 <ejinn1@naver.com>
2024-12-23 11:36:18 +0900

    Feat: add heart icon

--- src/components/Dashboard/Follower/FollowerStory/index.tsx
+++ src/components/Dashboard/Follower/FollowerStory/index.tsx
@@ -1,4 +1,7 @@
+import { FaHeart } from 'react-icons/fa6';
+
 import { FollowerTypes } from '@/mocks/followerData';
+import { cn } from '@/utils/className';
 import { formatDateToRelativeTime } from '@/utils/date';
 
 interface FollowerStoryProps {
@@ -6,9 +9,16 @@ interface FollowerStoryProps {
 }
 
 export const FollowerStory = ({ follower }: FollowerStoryProps) => {
+  const heartClass = cn(
+    'absolute right-8 top-8 size-28 p-4',
+    follower.likeStatus ? 'text-error' : 'text-custom-gray-200',
+  );
+
   return (
     <div className="shrink-0 snap-start">
-      <div className="size-120 rounded-20 bg-custom-white-300" />
+      <div className="relative size-120 rounded-20 bg-custom-white-300">
+        <FaHeart className={heartClass} />
+      </div>
       <div className="pt-8">
         <span className="mr-4 text-xs-medium">{follower.name}</span>
         <span className="text-xs-normal text-custom-gray-200">
