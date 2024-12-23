c4ad6e6473b181ad1d1e435aee0349545fa6408d
ejinn1 <ejinn1@naver.com>
2024-12-23 15:29:59 +0900

    Refactor: refactor skeleton logic in sidebar user

--- src/components/Sidebar/Profle/index.tsx
+++ src/components/Sidebar/Profle/index.tsx
@@ -21,8 +21,8 @@ export const Profile = () => {
   }, [isLoading]);
 
   return (
-    <div className="flex w-full gap-8 p-16">
-      {showSkeleton || isLoading ? (
+    <div className="flex min-h-53 w-full gap-8 p-16">
+      {showSkeleton ? (
         <>
           <Skeleton className="size-37 rounded-8" />
           <div className="flex flex-col gap-8 py-4">
@@ -32,14 +32,16 @@ export const Profile = () => {
         </>
       ) : (
         <>
-          <Image
-            src={profile}
-            alt="profile picture"
-            width={37}
-            height={37}
-            className="shrink-0 rounded-8"
-            priority
-          />
+          {!isLoading && (
+            <Image
+              src={profile}
+              alt="profile picture"
+              width={37}
+              height={37}
+              className="shrink-0 rounded-8"
+              priority
+            />
+          )}
           <div className="flex w-full justify-between">
             <div>
               <p className="text-sm-medium">{name}</p>
