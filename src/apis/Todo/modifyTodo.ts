ec3cde97c501542c629b9c69d790172521684cc5
beom <gb3187@naver.com>
2024-12-23 13:04:28 +0900

    Refactor: todoId 타입 변경 및 todoData 저장소 추가

--- src/apis/Todo/modifyTodo.ts
+++ src/apis/Todo/modifyTodo.ts
@@ -2,10 +2,7 @@ import { API_ENDPOINTS } from '@/constants/ApiEndpoints';
 import { ModifyTodosRequest } from '@/types/Todos/ModifyTodos/ModifyTodosRequest';
 import axiosInstance from '@/lib/axiosInstance';
 
-export const modifyTodo = async (
-  todoId: string | number,
-  data: ModifyTodosRequest,
-) => {
+export const modifyTodo = async (todoId: number, data: ModifyTodosRequest) => {
   try {
     const response = await axiosInstance.put(
       API_ENDPOINTS.TODOS.PUT(todoId),
