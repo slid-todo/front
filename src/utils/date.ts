898dad74ebaa83f92091655c0ea1d6264ca6c674
ejinn1 <ejinn1@naver.com>
2024-12-23 11:10:01 +0900

    Feat: create format date to relative time function

--- src/utils/date.ts
+++ src/utils/date.ts
@@ -7,3 +7,38 @@ export const formatDateToPoint = (dateString: string) => {
 
   return `${year}.${month}.${day}`;
 };
+
+export const formatDateToRelativeTime = (dateString: string): string => {
+  const date = new Date(dateString);
+  const now = new Date();
+  const timeDifference = date.getTime() - now.getTime();
+  const rtf = new Intl.RelativeTimeFormat('ko', { numeric: 'auto' });
+
+  const seconds = Math.floor(timeDifference / 1000);
+  if (Math.abs(seconds) < 60) {
+    return rtf.format(seconds, 'second');
+  }
+
+  const minutes = Math.floor(seconds / 60);
+  if (Math.abs(minutes) < 60) {
+    return rtf.format(minutes, 'minute');
+  }
+
+  const hours = Math.floor(minutes / 60);
+  if (Math.abs(hours) < 24) {
+    return rtf.format(hours, 'hour');
+  }
+
+  const days = Math.floor(hours / 24);
+  if (Math.abs(days) < 30) {
+    return rtf.format(days, 'day');
+  }
+
+  const months = Math.floor(days / 30);
+  if (Math.abs(months) < 12) {
+    return rtf.format(months, 'month');
+  }
+
+  const years = Math.floor(months / 12);
+  return rtf.format(years, 'year');
+};
