6a14041d3eb5f71ac2fc80ae688733e394a0a68d
beom <gb3187@naver.com>
2024-12-24 16:22:30 +0900

    Refactor: 할 일 수정 페이지 렌더링시 목표 탭 고정

--- src/components/TodoModal/TodoModalGoal/index.tsx
+++ src/components/TodoModal/TodoModalGoal/index.tsx
@@ -6,13 +6,21 @@ import { PLACEHOLDERS } from '@/constants/Placeholders';
 import { useGoalsQuery } from '@/hooks/apis/useGoalsQuery';
 import { useTodoDataStore } from '@/store/useTodoDataStore';
 import { Goal } from '@/types/Goals';
-
+import { useTodoModalStore } from '@/store/useTodoModalStore';
+import { cn } from '@/utils/className';
 import { DropdownIcon } from './DropdownIcon';
 
 export const TodoModalGoal = () => {
   const { goals } = useGoalsQuery();
   const { todoData, setTodoData } = useTodoDataStore();
+  const { todoType } = useTodoModalStore();
   const [isOpenDropdown, setIsOpenDropdown] = useState(false);
+
+  const inputClass = cn(
+    `flex items-center justify-between self-stretch 
+    ${isOpenDropdown && todoType !== '수정' ? 'rounded-t-12' : 'rounded-12'} 
+    bg-white pr-16 ${todoType === '수정' ? 'text-custom-gray-100' : ''}`,
+  );
 
   const handleDropdown = () => {
     setIsOpenDropdown(!isOpenDropdown);
@@ -30,9 +38,7 @@ export const TodoModalGoal = () => {
   return (
     <div className="relative flex flex-col items-start gap-12 self-stretch">
       <span className="text-base-semibold text-slate-800">목표</span>
-      <div
-        className={`flex items-center justify-between self-stretch ${isOpenDropdown ? 'rounded-t-12' : 'rounded-12'} bg-white pr-16`}
-      >
+      <div className={inputClass}>
         <Input
           className="cursor-pointer"
           placeholder={PLACEHOLDERS.TARGET}
@@ -42,16 +48,19 @@ export const TodoModalGoal = () => {
         />
         <DropdownIcon
           isOpenDropdown={isOpenDropdown}
+          hidden={todoType === '수정'}
           onClick={handleDropdown}
         />
       </div>
 
-      <Dropdown
-        dropdownData={goals}
-        onSelectItem={handleSelectItem}
-        isOpenDropdown={isOpenDropdown}
-        renderItem={renderDropdownItem}
-      />
+      {todoType !== '수정' && (
+        <Dropdown
+          dropdownData={goals}
+          onSelectItem={handleSelectItem}
+          isOpenDropdown={isOpenDropdown}
+          renderItem={renderDropdownItem}
+        />
+      )}
     </div>
   );
 };
