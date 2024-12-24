d357ace86d5c761cab394d98109465e00b00231b
beom <gb3187@naver.com>
2024-12-24 16:22:30 +0900

    Refactor: 할 일 수정 페이지 렌더링시 목표 탭 고정

--- src/components/TodoModal/TodoModalGoal/DropdownIcon/index.tsx
+++ src/components/TodoModal/TodoModalGoal/DropdownIcon/index.tsx
@@ -2,11 +2,13 @@ import { IoMdArrowDropdown } from 'react-icons/io';
 
 interface DropdownIconProps {
   isOpenDropdown: boolean;
+  hidden: boolean;
   onClick: () => void;
 }
 
 export const DropdownIcon = ({
   isOpenDropdown,
+  hidden,
   onClick,
 }: DropdownIconProps) => {
   const handleDropdown = () => {
@@ -16,7 +18,7 @@ export const DropdownIcon = ({
   return (
     <>
       <IoMdArrowDropdown
-        className={`size-24 cursor-pointer transition-transform duration-300 ${isOpenDropdown ? 'rotate-180' : ''}`}
+        className={`size-24 cursor-pointer transition-transform duration-300 ${isOpenDropdown ? 'rotate-180' : ''} ${hidden ? 'hidden' : ''}`}
         onClick={handleDropdown}
       />
     </>
