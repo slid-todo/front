1c754250e0f30243f6aaff6f43183faf80e2eb49
dudwns0213 <dudwns0213@gmail.com>
2024-12-25 21:55:49 +0900

    Feat: create ConfirmationModal

---
+++ src/components/ConfirmationModal/index.tsx
@@ -0,0 +1,41 @@
+import { Button } from '../common/Button/Button';
+import { ModalContent } from '../common/Modal';
+
+interface ConfirmationModalProps {
+  isOpen: boolean;
+  onClose: () => void;
+  onConfirm: () => void;
+  onCancel: () => void;
+  title?: string;
+  description?: string;
+  confirmText?: string;
+  cancelText?: string;
+}
+
+export const ConfirmationModal = (props: ConfirmationModalProps) => {
+  const {
+    isOpen,
+    onClose,
+    onConfirm,
+    onCancel,
+    title = '제목',
+    description = '이 작업을 진행하시겠습니까?',
+    confirmText = '예',
+    cancelText = '아니오',
+  } = props;
+
+  return (
+    <ModalContent isOpen={isOpen} onClose={onClose}>
+      <div className="flex flex-col items-center justify-center px-24">
+        <h1 className="mb-16 text-xl font-bold">{title}</h1>
+
+        <p className="mb-24">{description}</p>
+
+        <div className="flex w-full justify-between">
+          <Button onClick={onConfirm}>{confirmText}</Button>
+          <Button onClick={onCancel}>{cancelText}</Button>
+        </div>
+      </div>
+    </ModalContent>
+  );
+};
