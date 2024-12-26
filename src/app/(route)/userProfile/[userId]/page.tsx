51b72f0ea0cc27e42b07c6f49e002a576a5cb4b8
beom <gb3187@naver.com>
2024-12-26 14:43:42 +0900

    Refactor : 검색, 프로필 페이지 임시 반응형 적용

--- src/app/(route)/userProfile/[userId]/page.tsx
+++ src/app/(route)/userProfile/[userId]/page.tsx
@@ -11,7 +11,7 @@ export default async function userProfile({ params }: UserProfileProps) {
   const { userId } = await params;
 
   return (
-    <div className="flex min-h-screen w-screen flex-col gap-16 bg-custom-white-100 p-16">
+    <div className="flex min-h-screen w-screen flex-col gap-16 bg-custom-white-100 p-16 md:px-200 xl:px-400 2xl:px-650">
       <UserProfileHeader userId={userId} />
       <UserProfileContent />
     </div>
