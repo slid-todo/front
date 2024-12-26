6b0f0cb74ba6744126380c6f195d1deb24b348d1
beom <gb3187@naver.com>
2024-12-26 14:35:03 +0900

    Feat: 레이아웃 및 메타데이터 설정

--- src/app/(route)/userProfile/[userId]/layout.tsx
+++ src/app/(route)/userProfile/[userId]/layout.tsx
@@ -1,8 +1,8 @@
 import type { Metadata } from 'next';
 
 export const metadata: Metadata = {
-  title: 'Search Users or Goals',
-  description: 'Search Page',
+  title: 'User Profile',
+  description: 'User Profile Page',
 };
 
 export default function RootLayout({
