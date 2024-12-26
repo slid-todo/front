23b0ce8f969066073f4080959f039d03d5deccb1
dudwns0213 <dudwns0213@gmail.com>
2024-12-26 16:41:52 +0900

    Feat: useCertifiedTodo success시 router push

--- src/constants/Messages.ts
+++ src/constants/Messages.ts
@@ -3,6 +3,8 @@ export const ERROR_MESSAGE = 'Toast 메시지는 비어있을 수 없습니다.'
 export const TOAST_MESSAGES = {
   SAVE_SUCCESS: '임시 저장 되었습니다.',
   LOAD_SUCCESS: '불러오기가 완료되었습니다.',
+  CERTIFIED_SUCCESS: '인증에 성공하였습니다.',
+  CERTIFIED_ERROR: '인증을 다시 시도해주세요.',
   CERTIFIED_IMG: 'completeId가 설정되지 않았습니다.',
   CERTIFIED_COMPLETEID: '이미지가 선택되지 않았습니다.',
 };
@@ -10,7 +12,8 @@ export const TOAST_MESSAGES = {
 export const MODAL_MESSAGES = {
   SAVE_TITLE: '임시저장',
   LOAD_TITLE: '불러오기',
-  SAVE_CONFIRM_DESCRIPTION: '작성된 내용을 임시 저장하시겠습니까?',
+  SAVE_CONFIRM_DESCRIPTION:
+    '작성된 내용을 임시 저장하시겠습니까? 작성한 사진, 글이 저장됩니다.',
   LOAD_CONFIRM_DESCRIPTION: '임시 저장된 내용을 불러오시겠습니까?',
   CONFIRM_TEXT: '예',
   CANCEL_TEXT: '아니오',
