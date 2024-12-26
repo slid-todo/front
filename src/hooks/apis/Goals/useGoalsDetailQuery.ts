789826980fc6d938f346a6dbe34997ef6c03385c
ejinn1 <ejinn1@naver.com>
2024-12-26 16:29:27 +0900

    Rename: change name all goals query to goals detail query

--- src/hooks/apis/Goals/useGoalsDetailQuery.ts
+++ src/hooks/apis/Goals/useGoalsDetailQuery.ts
@@ -8,22 +8,25 @@ import { BaseResponse } from '@/types/response';
 
 import { GoalsResponse } from '../Dashboard/useTodosOfGoalsQuery';
 
-interface AllGoalsResponse extends BaseResponse {
+interface GoalsDetailResponse extends BaseResponse {
   data: {
     content: GoalsResponse[];
   };
 }
 
-const allGoalsOptions = (): UseQueryOptions<AllGoalsResponse, AxiosError> => ({
+const GoalsDetailResponse = (): UseQueryOptions<
+  GoalsDetailResponse,
+  AxiosError
+> => ({
   queryKey: [QUERY_KEYS.ALL_GOALS],
   queryFn: () =>
-    GET<AllGoalsResponse>(
+    GET<GoalsDetailResponse>(
       `${API_ENDPOINTS.GOAL.ALL_GOALS}?lastGoalId=0&size=5`,
     ),
 });
 
-export const useAllGoalsQuery = () => {
-  const { data, isLoading, isError, error } = useQuery(allGoalsOptions());
+export const useGoalsDetailQuery = () => {
+  const { data, isLoading, isError, error } = useQuery(GoalsDetailResponse());
   const goals = data?.data.content ?? [];
 
   return { goals, isLoading, isError, error };
