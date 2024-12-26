5d09e404d2eac42200ad2e1bc45645c0ef1ff02f
ejinn1 <ejinn1@naver.com>
2024-12-26 14:13:16 +0900

    Feat: add goals detail page

---
+++ src/components/Goals/GoalList/index.tsx
@@ -0,0 +1,28 @@
+'use client';
+
+import { ProgressLine } from '@/components/Dashboard/GoalList/GoalItem/ProgressLine';
+import { TodoList } from '@/components/Dashboard/GoalList/GoalItem/TodoList';
+import { useAllGoalsQuery } from '@/hooks/apis/Goals/useAllgoalsQuery';
+import { GoalHeader } from './GoalHeader';
+
+export const GoalList = () => {
+  const { goals } = useAllGoalsQuery();
+  console.log(goals);
+
+  return (
+    <div className="flex flex-col gap-16">
+      {goals.map((goal) => (
+        <div
+          key={goal.goalId}
+          className="relative w-full rounded-12 bg-white p-16 shadow-sm"
+        >
+          <GoalHeader title={goal.goalTitle} color={goal.goalColor} />
+          <ProgressLine percent={goal.progress} color={goal.goalColor} />
+          {goal.todos.map((todo) => (
+            <TodoList key={todo.todoId} todo={todo} color={goal.goalColor} />
+          ))}
+        </div>
+      ))}
+    </div>
+  );
+};
