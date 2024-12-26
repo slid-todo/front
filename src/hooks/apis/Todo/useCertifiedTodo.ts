2426cb3daabd61d29aa871cc7ed9f14bf464e138
dudwns0213 <dudwns0213@gmail.com>
2024-12-26 16:41:52 +0900

    Feat: useCertifiedTodo success시 router push

--- src/hooks/apis/Todo/useCertifiedTodo.ts
+++ src/hooks/apis/Todo/useCertifiedTodo.ts
@@ -4,10 +4,12 @@ import {
   useQueryClient,
 } from '@tanstack/react-query';
 import { AxiosError, AxiosResponse } from 'axios';
+import router from 'next/router';
 import { notify } from '@/store/useToastStore';
 import { QUERY_KEYS } from '@/constants/QueryKeys';
 import { useTodoModalStore } from '@/store/useTodoModalStore';
 import { certifiedTodo } from '@/apis/Todo/certifiedTodo';
+import { TOAST_MESSAGES } from '@/constants/Messages';
 
 export interface CertifiedTodoRequest {
   completePicBase64: string;
@@ -33,15 +35,16 @@ export const useCertifiedTodo = (): UseMutationResult<
     mutationFn: ({ completeId, data }: CertifiedTodoProps) =>
       certifiedTodo(completeId, data),
     onSuccess: () => {
-      notify('success', '수정에 성공하였습니다', 3000);
+      notify('success', TOAST_MESSAGES.CERTIFIED_SUCCESS, 3000);
       queryClient.invalidateQueries({ queryKey: [QUERY_KEYS.CERTIFIED_TODO] });
       queryClient.invalidateQueries({
         queryKey: [QUERY_KEYS.CERTIFIED_RECENT_TODO],
       });
       close();
+      router.push('/todos');
     },
     onError: (error: AxiosError) => {
-      notify('error', '수정에 실패하였습니다', 3000);
+      notify('error', TOAST_MESSAGES.CERTIFIED_ERROR, 3000);
       console.error('Error creating todo:', error.message);
     },
   });
