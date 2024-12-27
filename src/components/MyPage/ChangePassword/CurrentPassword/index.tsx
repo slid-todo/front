3240cc0527b31b7d202361f176a466268a015ae2
beom <gb3187@naver.com>
2024-12-27 13:05:36 +0900

    Feat: 비밀번호 변경 컴포넌트 퍼블리싱, 분리, react-hook-form 적용, 유효성 검사

---
+++ src/components/MyPage/ChangePassword/CurrentPassword/index.tsx
@@ -0,0 +1,31 @@
+import { useState } from 'react';
+import { Input } from '@/components/common/Input';
+import { PLACEHOLDERS } from '@/constants/Placeholders';
+import { ChangePasswordProps } from '@/types/Auth/ChangePasswordProps';
+import { VisibilityIcon } from '@/components/AuthPage/VisibilityIcon';
+
+export const CurrentPassword = ({ register, error }: ChangePasswordProps) => {
+  const [isVisible, setIsVisible] = useState(false);
+
+  const handleClickIcon = () => {
+    setIsVisible(!isVisible);
+  };
+
+  return (
+    <div className="flex flex-col gap-8">
+      <span className="text-sm-normal">현재 비밀번호</span>
+      <div className="flex w-full items-center justify-between gap-10 rounded-12 bg-white pr-24">
+        <Input
+          type={isVisible ? 'text' : 'password'}
+          placeholder={PLACEHOLDERS.CURRENT_PASSWORD}
+          {...register('currentPassword', {
+            required: '현재 비밀번호를 입력해주세요',
+          })}
+        />
+        <VisibilityIcon isVisible={isVisible} onClick={handleClickIcon} />
+      </div>
+
+      <span className="text-xs-normal text-error">{error?.message}</span>
+    </div>
+  );
+};
