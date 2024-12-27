d1613b30519118c9f7ffaa02fcf0f5b7e168d6b8
ejinn1 <ejinn1@naver.com>
2024-12-27 10:29:00 +0900

    Feat: add infinite scrollling to todos og goals

--- src/components/Dashboard/GoalList/index.tsx
+++ src/components/Dashboard/GoalList/index.tsx
@@ -1,56 +1,40 @@
 'use client';
 
-import { useEffect, useRef, useState } from 'react';
+import { useEffect, useRef } from 'react';
 
 import { Card } from '@/components/common/Card';
 import { DashboardItemContainer } from '@/components/Dashboard/DashboardItemContainer';
 import { GoalItem } from '@/components/Dashboard/GoalList/GoalItem';
 
-import {
-  GoalsResponse,
-  useTodosOfGoalsQuery,
-} from '@/hooks/apis/Dashboard/useTodosOfGoalsQuery';
+import { useTodosOfGoalsQuery } from '@/hooks/apis/Dashboard/useTodosOfGoalsQuery';
 
 export const GoalList = () => {
   const observerRef = useRef<HTMLDivElement | null>(null);
 
-  const [goals, setGoals] = useState<GoalsResponse[]>([]);
-  const [lastGoalId, setLastGoalId] = useState(0);
-
-  const { goals: addGoals, isLoading } = useTodosOfGoalsQuery(lastGoalId);
+  const { goals, fetchNextPage, isLoading } = useTodosOfGoalsQuery();
 
   useEffect(() => {
-    if (!isLoading && addGoals.length > 0) {
-      setGoals((prev) => {
-        const newGoals = addGoals.filter(
-          (goal) => !prev.some((prevGoal) => prevGoal.goalId === goal.goalId),
-        );
-        return [...prev, ...newGoals];
-      });
-    }
-  }, [addGoals, isLoading]);
+    if (!observerRef.current) return;
 
-  useEffect(() => {
-    const observer = new IntersectionObserver(
-      (entries) => {
-        const lastEntry = entries[0];
-        if (lastEntry.isIntersecting && !isLoading) {
-          setLastGoalId(goals[goals.length - 1]?.goalId || 0);
-        }
-      },
-      { threshold: 1.0 },
-    );
+    const observerCallback: IntersectionObserverCallback = (entries) => {
+      const lastEntry = entries[0];
+      if (lastEntry.isIntersecting && !isLoading) {
+        fetchNextPage();
+      }
+    };
+
+    const observer = new IntersectionObserver(observerCallback, {
+      threshold: 1.0,
+    });
 
     if (observerRef.current) {
       observer.observe(observerRef.current);
     }
 
-    return () => {
-      if (observerRef.current) {
-        observer.unobserve(observerRef.current);
-      }
-    };
-  }, [goals, isLoading]);
+    observer.observe(observerRef.current);
+
+    return () => observer.disconnect();
+  }, [fetchNextPage, isLoading]);
 
   return (
     <DashboardItemContainer title="목표 별 할 일">
