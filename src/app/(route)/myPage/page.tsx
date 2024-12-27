e0209bf4ceca59d0318b469e01444c95209918c9
beom <gb3187@naver.com>
2024-12-27 12:27:01 +0900

    Feat: 마이 페이지 퍼블리싱 및 컴포넌트 분리

---
+++ src/app/(route)/myPage/page.tsx
@@ -0,0 +1,24 @@
+import type { Metadata } from 'next';
+import { Header } from '@/components/common/Header';
+import { MyProfile } from '@/components/MyPage/MyProfile';
+import { MyFollow } from '@/components/MyPage/MyFollow';
+import { ChangePassword } from '@/components/MyPage/ChangePassword';
+
+export const metadata: Metadata = {
+  title: 'My page',
+  description: 'My page',
+};
+
+export default function myPage() {
+  return (
+    <>
+      <Header />
+      <div className="flex min-h-screen w-screen flex-col gap-16 bg-custom-white-100 p-16 pt-70 md:px-200 xl:px-400 2xl:px-650">
+        <MyProfile />
+        <MyFollow />
+        <hr className="my-8 h-6 w-full bg-custom-white-200" />
+        <ChangePassword />
+      </div>
+    </>
+  );
+}
