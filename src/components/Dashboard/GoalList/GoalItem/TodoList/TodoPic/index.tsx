3d0b0263cf818dcadbf03b7932613cb360489591
ejinn1 <ejinn1@naver.com>
2024-12-28 13:19:18 +0900

    Feat: add complete image in board

--- src/components/Dashboard/GoalList/GoalItem/TodoList/TodoPic/index.tsx
+++ src/components/Dashboard/GoalList/GoalItem/TodoList/TodoPic/index.tsx
@@ -1,14 +1,35 @@
+import { useState } from 'react';
+
+import Image from 'next/image';
+
+import { Skeleton } from '@/components/common/Skeleton';
+
 interface TodoItemProps {
   index: number;
   color: string;
+  pic: string;
 }
 
-export const TodoPic = ({ index, color }: TodoItemProps) => {
+export const TodoPic = ({ index, color, pic }: TodoItemProps) => {
+  const [isLoaded, setIsLoaded] = useState(pic === '' || false);
+
   return (
     <div
-      className="flex-center aspect-square rounded-16"
+      className="flex-center relative aspect-square rounded-16"
       style={{ backgroundColor: color }}
     >
+      {!isLoaded && pic && (
+        <Skeleton className="absolute inset-0 size-full rounded-16" />
+      )}
+      {pic && (
+        <Image
+          src={pic}
+          fill
+          alt="인증 사진"
+          onLoad={() => setIsLoaded(true)}
+          className="rounded-16 object-cover"
+        />
+      )}
       {index + 1}
     </div>
   );
