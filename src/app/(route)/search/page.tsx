f5fcee314ba6e2eb1cefb1768b4976caf72021f1
beom <gb3187@naver.com>
2024-12-29 17:35:47 +0900

    Refactor: 검색 후 페이지를 다시 들어왔을때 검색 상태 초기화

--- src/app/(route)/search/page.tsx
+++ src/app/(route)/search/page.tsx
@@ -1,8 +1,19 @@
+'use client';
+
+import { useEffect } from 'react';
 import { SearchContent } from '@/components/Search/SearchContent';
 import { SearchHeader } from '@/components/Search/SearchHeader';
 import { SearchInput } from '@/components/Search/SearchInput';
+import { useSearchStore } from '@/store/useSearchStore';
 
 export default function SearchPage() {
+  const { setSearchFilter, setSearchKeyword } = useSearchStore();
+
+  useEffect(() => {
+    setSearchFilter('유저명');
+    setSearchKeyword('');
+  }, [setSearchFilter, setSearchKeyword]);
+
   return (
     <div className="flex min-h-screen w-screen flex-col gap-16 bg-custom-white-100 p-16 md:px-200 xl:px-400 2xl:px-650">
       <SearchHeader />
