1cefccc77cb88eae7c0c63d800faf5c6356f9438
ejinn1 <ejinn1@naver.com>
2024-12-29 17:53:59 +0900

    Feat: add optimitstic update to edit goal title mutation

--- src/hooks/apis/Goals/useEditGoalTitleMutation.ts
+++ src/hooks/apis/Goals/useEditGoalTitleMutation.ts
@@ -4,33 +4,71 @@ import { PUT } from '@/apis/services/httpMethod';
 import { API_ENDPOINTS } from '@/constants/ApiEndpoints';
 import { QUERY_KEYS } from '@/constants/QueryKeys';
 import { notify } from '@/store/useToastStore';
-import { BaseResponse } from '@/types/response';
+import {
+  BaseInfiniteQueryResponse,
+  EditGoalTitleResponse,
+  GoalsDetailResponse,
+} from '@/types/response';
 
 interface PostDataTypes {
   title: string;
 }
 
-interface EditGoalTitleResponse extends BaseResponse {
-  data: {
-    goalId: number;
-  };
-}
-
-export const useEditGoalTitleMutation = (goalId: number) => {
+export const useEditGoalTitleMutation = (editGoalId: number) => {
   const queryClient = useQueryClient();
 
   return useMutation({
     mutationFn: (postData: PostDataTypes) =>
       PUT<EditGoalTitleResponse, PostDataTypes>(
-        API_ENDPOINTS.GOAL.GOAL(goalId),
+        API_ENDPOINTS.GOAL.GOAL(editGoalId),
         postData,
       ),
-    onSuccess: () => {
+    onMutate: async (postData) => {
+      const prev = queryClient.getQueriesData({
+        queryKey: [QUERY_KEYS.ALL_GOALS],
+      });
+
+      await queryClient.cancelQueries({ queryKey: [QUERY_KEYS.ALL_GOALS] });
+
+      queryClient.setQueryData(
+        [QUERY_KEYS.ALL_GOALS],
+        (
+          oldData: BaseInfiniteQueryResponse<GoalsDetailResponse[]> | undefined,
+        ) => {
+          if (!oldData) return undefined;
+
+          return {
+            pageParams: oldData.pageParams,
+            pages: oldData.pages.map((page) => ({
+              ...page,
+              data: {
+                ...page.data,
+                content: page.data.content.map((goal) =>
+                  goal.goalId === editGoalId
+                    ? { ...goal, goalTitle: postData.title }
+                    : goal,
+                ),
+              },
+            })),
+          };
+        },
+      );
+
+      return { prev };
+    },
+    onSettled: () => {
       queryClient.invalidateQueries({ queryKey: [QUERY_KEYS.GOALS] });
       queryClient.invalidateQueries({ queryKey: [QUERY_KEYS.ALL_GOALS] });
+      queryClient.invalidateQueries({ queryKey: [QUERY_KEYS.TODOS_OF_GOALS] });
+    },
+    onSuccess: () => {
       notify('success', '목표 수정에 성공했습니다.', 3000);
     },
-    onError: (error) => {
+    onError: (error, _, context) => {
+      queryClient.setQueriesData(
+        { queryKey: [QUERY_KEYS.ALL_GOALS] },
+        context?.prev,
+      );
       console.error(error.message);
       notify('error', '목표 수정에 실패했습니다.', 3000);
     },
