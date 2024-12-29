7affa67e69074548f985f1490bb39683338bac53
beom <gb3187@naver.com>
2024-12-29 17:20:22 +0900

    Refactor: header 컴포넌트 검색페이지 경로 이동

--- src/components/common/Header/index.tsx
+++ src/components/common/Header/index.tsx
@@ -1,7 +1,8 @@
 'use client';
 
 import { FaBarsStaggered, FaBell } from 'react-icons/fa6';
-
+import { FaSearch } from 'react-icons/fa';
+import { useRouter } from 'next/navigation';
 import { useSidebarStore } from '@/store/useSidebarStore';
 
 interface HeaderProps {
@@ -10,6 +11,11 @@ interface HeaderProps {
 
 export const Header = ({ title = '' }: HeaderProps) => {
   const { open } = useSidebarStore();
+  const router = useRouter();
+
+  const handleClickSearch = () => {
+    router.push('/search');
+  };
 
   return (
     <div className="fixed left-0 top-0 z-10 flex h-48 w-full items-center justify-between bg-white px-16 md:hidden">
@@ -21,7 +27,13 @@ export const Header = ({ title = '' }: HeaderProps) => {
         />
         <p className="ml-16 text-base-semibold">{title}</p>
       </div>
-      <FaBell className="right-0 size-24 cursor-pointer p-4 text-primary-100" />
+      <div className="flex gap-8">
+        <FaSearch
+          className="size-24 cursor-pointer p-4 text-primary-100"
+          onClick={handleClickSearch}
+        />
+        <FaBell className="right-0 size-24 cursor-pointer p-4 text-primary-100" />
+      </div>
     </div>
   );
 };
