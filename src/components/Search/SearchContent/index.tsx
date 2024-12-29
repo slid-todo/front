6822294020cc57b7b214dc1d7bb2666a22aee9f0
beom <gb3187@naver.com>
2024-12-29 17:24:03 +0900

    Feat: 상태 변경에 따른 검색 리패치, 에러처리, 컴포넌트 분리

--- src/components/Search/SearchContent/index.tsx
+++ src/components/Search/SearchContent/index.tsx
@@ -1,64 +1,47 @@
 'use client';
 
-import Image from 'next/image';
-import { FaFlag } from 'react-icons/fa6';
+import { useEffect } from 'react';
+
 import { useSearchStore } from '@/store/useSearchStore';
-import { Goal } from '@/types/Goals';
-import {
-  SearchResponseData,
-  useSearchQuery,
-} from '@/hooks/apis/Search/useSearchQuery';
+import { useSearchQuery } from '@/hooks/apis/Search/useSearchQuery';
+
+import { SearchUserList } from './SearchUserList';
+import { SearchGoalList } from './SearchGoalList';
 
 export const SearchContent = () => {
   const { searchFilter, searchKeyword } = useSearchStore();
-  const { data: searchData } = useSearchQuery({
+  const {
+    data: searchData,
+    refetch,
+    isError,
+    error,
+  } = useSearchQuery({
     searchField: searchFilter,
     keyword: searchKeyword,
   });
 
+  useEffect(() => {
+    if (searchKeyword || searchFilter) {
+      refetch();
+    }
+  }, [searchKeyword, searchFilter, refetch]);
+
   return (
     <div>
       {searchFilter === '유저명' ? (
-        <ul className="flex flex-col gap-16">
-          {searchData?.data.map((item: SearchResponseData) => (
-            <li className="flex w-full items-center gap-8" key={item.name}>
-              <Image
-                width={48}
-                height={48}
-                className="flex size-48 rounded-90"
-                src={item.profilePic}
-                alt="프로필 사진"
-              />
-              <span className="text-base-medium">{item.name}</span>
-            </li>
-          ))}
-        </ul>
+        <SearchUserList
+          isError={isError}
+          error={error}
+          searchData={searchData}
+          keyword={searchKeyword}
+        />
       ) : (
-        <ul className="flex flex-col gap-16">
-          {searchData?.data.map((item: SearchResponseData) => (
-            <li className="flex w-full flex-col" key={item.name}>
-              <div className="flex w-full items-center gap-8">
-                <Image
-                  width={48}
-                  height={48}
-                  className="flex size-48 rounded-90"
-                  src={item.profilePic}
-                  alt="프로필 사진"
-                />
-                <span className="text-base-medium">{item.name}</span>
-              </div>
-              {item.goals.map((item: Goal) => (
-                <div
-                  className="flex items-start gap-8 py-16 pl-48 pr-16"
-                  key={item.goalId}
-                >
-                  <FaFlag className="size-18" style={{ fill: item.color }} />
-                  <span className="text-base-medium">{item.goalTitle}</span>
-                </div>
-              ))}
-            </li>
-          ))}
-        </ul>
+        <SearchGoalList
+          isError={isError}
+          error={error}
+          searchData={searchData}
+          keyword={searchKeyword}
+        />
       )}
     </div>
   );
