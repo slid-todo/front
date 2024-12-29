75706a40f75b896b756cdc0c68e4a17301da4b82
ejinn1 <ejinn1@naver.com>
2024-12-29 14:08:05 +0900

    Feat: add goal page infinite query

--- src/hooks/apis/Goals/useGoalsDetailQuery.ts
+++ src/hooks/apis/Goals/useGoalsDetailQuery.ts
@@ -1,4 +1,7 @@
-import { useQuery, UseQueryOptions } from '@tanstack/react-query';
+import {
+  useInfiniteQuery,
+  UseInfiniteQueryOptions,
+} from '@tanstack/react-query';
 import { AxiosError } from 'axios';
 
 import { GET } from '@/apis/services/httpMethod';
@@ -6,28 +9,38 @@ import { API_ENDPOINTS } from '@/constants/ApiEndpoints';
 import { QUERY_KEYS } from '@/constants/QueryKeys';
 import { BaseResponse } from '@/types/response';
 
+import { BasePageableTypes } from '@/types/pageable';
 import { GoalsResponse } from '../Dashboard/useTodosOfGoalsQuery';
 
 interface GoalsDetailResponse extends BaseResponse {
-  data: {
-    content: GoalsResponse[];
-  };
+  data: BasePageableTypes<GoalsResponse[]>;
 }
 
-const GoalsDetailResponse = (): UseQueryOptions<
+interface InfiniteQueryResponse {
+  pageParams: number[];
+  pages: GoalsDetailResponse[];
+}
+
+const GoalsDetailResponse = (): UseInfiniteQueryOptions<
   GoalsDetailResponse,
-  AxiosError
+  AxiosError,
+  InfiniteQueryResponse
 > => ({
   queryKey: [QUERY_KEYS.ALL_GOALS],
-  queryFn: () =>
+  queryFn: ({ pageParam = 0 }) =>
     GET<GoalsDetailResponse>(
-      `${API_ENDPOINTS.GOAL.ALL_GOALS}?lastGoalId=0&size=5`,
+      `${API_ENDPOINTS.GOAL.ALL_GOALS}?lastGoalId=${pageParam}&size=3`,
     ),
+  getNextPageParam: (lastPage) => {
+    const nextCursor = lastPage.data.nextCursor;
+    return nextCursor !== 0 ? nextCursor : undefined;
+  },
+  initialPageParam: 0,
 });
 
 export const useGoalsDetailQuery = () => {
-  const { data, ...etc } = useQuery(GoalsDetailResponse());
-  const goals = data?.data.content ?? [];
+  const { data, ...etc } = useInfiniteQuery(GoalsDetailResponse());
+  const goals = data?.pages.flatMap((page) => page.data.content) ?? [];
 
   return { goals, ...etc };
 };
