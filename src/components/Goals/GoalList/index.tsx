d03ce23af05a6c2e3facad1004694d5e49e70271
ejinn1 <ejinn1@naver.com>
2024-12-29 14:57:36 +0900

    Refactor: create infinite scroll custom hook

--- src/components/Goals/GoalList/index.tsx
+++ src/components/Goals/GoalList/index.tsx
@@ -1,41 +1,16 @@
 'use client';
 
-import { useEffect, useRef } from 'react';
-
 import { ProgressLine } from '@/components/Dashboard/GoalList/GoalItem/ProgressLine';
 import { TodoList } from '@/components/Dashboard/GoalList/GoalItem/TodoList';
 
 import { useGoalsDetailQuery } from '@/hooks/apis/Goals/useGoalsDetailQuery';
 
+import { useInfiniteScroll } from '@/hooks/useInfiniteScroll';
 import { GoalHeader } from './GoalHeader';
 
 export const GoalList = () => {
-  const observerRef = useRef<HTMLDivElement | null>(null);
-
   const { goals, fetchNextPage, isLoading } = useGoalsDetailQuery();
-
-  useEffect(() => {
-    if (!observerRef.current) return;
-
-    const observerCallback: IntersectionObserverCallback = (entries) => {
-      const lastEntry = entries[0];
-      if (lastEntry.isIntersecting && !isLoading) {
-        fetchNextPage();
-      }
-    };
-
-    const observer = new IntersectionObserver(observerCallback, {
-      threshold: 1.0,
-    });
-
-    if (observerRef.current) {
-      observer.observe(observerRef.current);
-    }
-
-    observer.observe(observerRef.current);
-
-    return () => observer.disconnect();
-  }, [fetchNextPage, isLoading]);
+  const { observerRef } = useInfiniteScroll({ fetchNextPage, isLoading });
 
   return (
     <div className="flex flex-col gap-16">
