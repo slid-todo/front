7a38f60c6c4a652db8efcbd8247fca2100b739dd
beom <gb3187@naver.com>
2024-12-30 01:40:53 +0900

    Refactor: 헤더 검색 아이콘 제거

--- src/components/common/Header/index.tsx
+++ src/components/common/Header/index.tsx
@@ -1,8 +1,6 @@
 'use client';
 
 import { FaBarsStaggered, FaBell } from 'react-icons/fa6';
-import { FaSearch } from 'react-icons/fa';
-import { useRouter } from 'next/navigation';
 import { useSidebarStore } from '@/store/useSidebarStore';
 
 interface HeaderProps {
@@ -11,11 +9,6 @@ interface HeaderProps {
 
 export const Header = ({ title = '' }: HeaderProps) => {
   const { open } = useSidebarStore();
-  const router = useRouter();
-
-  const handleClickSearch = () => {
-    router.push('/search');
-  };
 
   return (
     <div className="fixed left-0 top-0 z-10 flex h-48 w-full items-center justify-between bg-white px-16 md:hidden">
@@ -27,13 +20,7 @@ export const Header = ({ title = '' }: HeaderProps) => {
         />
         <p className="ml-16 text-base-semibold">{title}</p>
       </div>
-      <div className="flex gap-8">
-        <FaSearch
-          className="size-24 cursor-pointer p-4 text-primary-100"
-          onClick={handleClickSearch}
-        />
-        <FaBell className="right-0 size-24 cursor-pointer p-4 text-primary-100" />
-      </div>
+      <FaBell className="right-0 size-24 cursor-pointer p-4 text-primary-100" />
     </div>
   );
 };
