fe8423cd7215fe87a5c9345fedb6fbe3cdc36e4b
ejinn1 <ejinn1@naver.com>
2024-12-30 15:47:47 +0900

    Refactor: add goal page skeleton

---
+++ src/components/Skeletons/GoalListSkeleton/index.tsx
@@ -0,0 +1,11 @@
+import { Skeleton } from '@/components/common/Skeleton';
+
+export const GoalListSkeleon = () => {
+  return (
+    <>
+      <Skeleton className="h-200 rounded-12" />
+      <Skeleton className="h-160 rounded-12" />
+      <Skeleton className="h-160 rounded-12" />
+    </>
+  );
+};
