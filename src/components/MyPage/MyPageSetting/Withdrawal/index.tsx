85a22fd3a76b2d22bd93cc5c531c4e40a3e7fb26
beom <gb3187@naver.com>
2024-12-30 14:19:15 +0900

    Feat: 회원탈퇴 모달 구현

--- src/components/MyPage/MyPageSetting/Withdrawal/index.tsx
+++ src/components/MyPage/MyPageSetting/Withdrawal/index.tsx
@@ -1,7 +1,26 @@
+import { useState } from 'react';
+import { WithdrawalModal } from './WithdrawalModal';
+
 export const Withdrawal = () => {
+  const [isOpenModal, setIsOpenModal] = useState(false);
+
+  const handleModalOpen = () => {
+    setIsOpenModal(true);
+  };
+
+  const handleModalClose = () => {
+    setIsOpenModal(false);
+  };
+
   return (
-    <div className="flex items-center justify-between py-16">
-      <span className="cursor-pointer text-sm-normal">회원탈퇴</span>
-    </div>
+    <>
+      <div
+        className="flex items-center justify-between py-16"
+        onClick={handleModalOpen}
+      >
+        <span className="cursor-pointer text-sm-normal">회원탈퇴</span>
+      </div>
+      <WithdrawalModal isOpen={isOpenModal} onClose={handleModalClose} />
+    </>
   );
 };
