78575f126d9aaea05cd7234dcbeb4b3238266dc4
dudwns0213 <dudwns0213@gmail.com>
2025-01-02 08:35:38 +0900

    Feat: Like 로직 추가

--- src/components/Follows/Post/PostLike.tsx
+++ src/components/Follows/Post/PostLike.tsx
@@ -1,19 +1,37 @@
 import { FaRegHeart, FaHeart } from 'react-icons/fa6';
+import { useCreateLike } from '@/hooks/apis/Likes/useCreateLikeQuery';
+import { useDeleteLike } from '@/hooks/apis/Likes/useDeleteLikeQuery';
 
 interface PostLikeProps {
+  completeId: number;
   likeStatus: boolean;
   likeCount: number;
 }
 
-export function PostLike(props: PostLikeProps) {
-  const { likeStatus, likeCount } = props;
+export function PostLike({ completeId, likeStatus, likeCount }: PostLikeProps) {
+  const { mutate: createLike } = useCreateLike();
+  const { mutate: deleteLike } = useDeleteLike();
+
+  const handleClickLike = () => {
+    if (likeStatus) {
+      deleteLike(completeId);
+    } else {
+      createLike(completeId);
+    }
+  };
 
   return (
     <div className="flex items-center">
       {likeStatus ? (
-        <FaHeart className="m-12 size-22 bg-sub-pink" />
+        <FaHeart
+          className="ml-16 mr-12 size-22 cursor-pointer fill-sub-pink"
+          onClick={handleClickLike}
+        />
       ) : (
-        <FaRegHeart className="ml-16 mr-12 size-22" />
+        <FaRegHeart
+          className="ml-16 mr-12 size-22 cursor-pointer"
+          onClick={handleClickLike}
+        />
       )}
       <div className="text-sm-normal text-custom-gray-200">{likeCount}</div>
     </div>
