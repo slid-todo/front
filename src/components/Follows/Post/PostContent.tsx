021b8c12671e6e96c9b4c6fa645fadf18ad66d43
dudwns0213 <dudwns0213@gmail.com>
2025-01-02 08:03:51 +0900

    Style: Content css

--- src/components/Follows/Post/PostContent.tsx
+++ src/components/Follows/Post/PostContent.tsx
@@ -2,10 +2,10 @@ interface PostContentProps {
   text: string;
 }
 
-export function PostContent({ text }: PostContentProps) {
+export function PostContent(props: PostContentProps) {
+  const { text } = props;
+
   return (
-    <div style={{ marginBottom: '8px' }}>
-      <p>{text}</p>
-    </div>
+    <div className="mx-16 my-8 text-sm-normal text-custom-gray-300">{text}</div>
   );
 }
