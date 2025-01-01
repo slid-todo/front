f37075dae7a817c24a2783cc83b0fde95621c6be
beom <gb3187@naver.com>
2025-01-01 13:23:09 +0900

    Refactor: 데이터 적용 오류 수정

--- src/components/TodosDetail/TodosDetailContent/TodoDocs/index.tsx
+++ src/components/TodosDetail/TodosDetailContent/TodoDocs/index.tsx
@@ -18,7 +18,7 @@ export const TodoDocs = ({ todoLink, todoPic }: TodoDocsProps) => {
           height={340}
           className="w-full rounded-16"
           priority
-          src="https://slid-todo.s3.ap-northeast-2.amazonaws.com/a19c4793-d33b-4be6-9f65-097bed5a6709_testmouse1.png"
+          src={todoPic}
           alt="프로필 사진"
         />
       )}
@@ -26,7 +26,7 @@ export const TodoDocs = ({ todoLink, todoPic }: TodoDocsProps) => {
         <div className="flex items-center gap-8 rounded-8 bg-custom-white-200 p-12">
           <FaLink className="size-24 shrink-0 cursor-pointer text-primary-100" />
           <span className="truncate text-sm-normal text-custom-gray-100">
-            링크링크링크링크ddddddddddddddddddddddddddddddddddddddddddddddddddddddd
+            {todoLink}
           </span>
         </div>
       )}
