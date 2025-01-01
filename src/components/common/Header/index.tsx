c5caa83ec1a7d6213d3d7baa042ed058c0fb5455
dudwns0213 <dudwns0213@gmail.com>
2025-01-02 03:17:00 +0900

    Feat: follows header true

--- src/components/common/Header/index.tsx
+++ src/components/common/Header/index.tsx
@@ -1,14 +1,20 @@
 'use client';
 
-import { FaBarsStaggered, FaBell } from 'react-icons/fa6';
+import { FaBarsStaggered, FaBell, FaMagnifyingGlass } from 'react-icons/fa6';
+import router from 'next/router';
 import { useSidebarStore } from '@/store/useSidebarStore';
 
 interface HeaderProps {
   title?: string;
+  search?: boolean;
 }
 
-export const Header = ({ title = '' }: HeaderProps) => {
+export const Header = ({ title = '', search = false }: HeaderProps) => {
   const { open } = useSidebarStore();
+
+  const handleClickMagnifyingGlass = () => {
+    router.push('/follows');
+  };
 
   return (
     <div className="fixed left-0 top-0 z-10 flex h-48 w-full items-center justify-between bg-white px-16 md:hidden">
@@ -20,7 +26,15 @@ export const Header = ({ title = '' }: HeaderProps) => {
         />
         <p className="ml-16 text-base-semibold">{title}</p>
       </div>
-      <FaBell className="right-0 size-24 cursor-pointer p-4 text-primary-100" />
+      <div className="flex">
+        {search && (
+          <FaMagnifyingGlass
+            className="mr-8 size-24 cursor-pointer p-4 text-primary-100"
+            onClick={handleClickMagnifyingGlass}
+          />
+        )}
+        <FaBell className="right-0 size-24 cursor-pointer p-4 text-primary-100" />
+      </div>
     </div>
   );
 };
