bab13fa33ebfe517bc102f2ed24595254eba2fbe
dudwns0213 <dudwns0213@gmail.com>
2025-01-02 08:03:14 +0900

    Style: Comments svg 적용 및 css

--- src/components/Follows/Post/PostComments.tsx
+++ src/components/Follows/Post/PostComments.tsx
@@ -1,9 +1,16 @@
-import React from 'react';
+import { FaRegComment } from 'react-icons/fa6';
 
 interface PostCommentsProps {
   commentCount: number;
 }
 
-export function PostComments({ commentCount }: PostCommentsProps) {
-  return <div style={{ marginBottom: '8px' }}>댓글 수: {commentCount}</div>;
+export function PostComments(props: PostCommentsProps) {
+  const { commentCount } = props;
+
+  return (
+    <div className="flex items-center">
+      <FaRegComment className="ml-16 mr-12 size-22" />
+      <div className="text-sm-normal text-custom-gray-200">{commentCount}</div>
+    </div>
+  );
 }
