65b642462d34255d75086d6f31a77a9e751a5395
ejinn1 <ejinn1@naver.com>
2025-01-02 11:18:08 +0900

    Refactor: move no data text to message folder

--- src/components/Goals/GoalList/index.tsx
+++ src/components/Goals/GoalList/index.tsx
@@ -5,11 +5,13 @@ import { TodoList } from '@/components/Dashboard/GoalList/GoalItem/TodoList';
 
 import { useGoalsDetailQuery } from '@/hooks/apis/Goals/useGoalsDetailQuery';
 
+import { Button } from '@/components/common/Button/Button';
 import { NoDataText } from '@/components/common/NoDataText';
 import { Spinner } from '@/components/common/Spinner';
 import { GoalListSkeleon } from '@/components/Skeletons/GoalListSkeleton';
-import { NO_DATA_TEXT } from '@/constants/NoDataText';
+import { NO_DATA_MESSAGES } from '@/constants/Messages';
 import { useInfiniteScroll } from '@/hooks/useInfiniteScroll';
+import { useSidebarStore } from '@/store/useSidebarStore';
 import { compareDates } from '@/utils/date';
 import { GoalHeader } from './GoalHeader';
 
@@ -17,6 +19,8 @@ export const GoalList = () => {
   const { goals, fetchNextPage, isLoading, isFetchingNextPage } =
     useGoalsDetailQuery();
   const { observerRef } = useInfiniteScroll({ fetchNextPage, isLoading });
+
+  const { open: openSidebar } = useSidebarStore();
 
   return (
     <>
@@ -58,7 +62,12 @@ export const GoalList = () => {
           <div ref={observerRef} style={{ height: '1px' }} />
         </div>
       ) : (
-        <NoDataText text={NO_DATA_TEXT.NO_GOAL} />
+        <div className="flex-center h-full flex-col gap-16">
+          <NoDataText text={NO_DATA_MESSAGES.NO_GOAL} />
+          <Button onClick={openSidebar} size="medium">
+            새 목표 등록
+          </Button>
+        </div>
       )}
     </>
   );
