ff86cb35c49ed4cf45801429d7846763c2e0917c
beom <gb3187@naver.com>
2025-01-02 11:49:42 +0900

    Refactor: 메타데이터 제거

--- src/app/(route)/userProfile/[userId]/layout.tsx
+++ src/app/(route)/userProfile/[userId]/layout.tsx
@@ -1,10 +1,3 @@
-import type { Metadata } from 'next';
-
-export const metadata: Metadata = {
-  title: 'User Profile',
-  description: 'User Profile Page',
-};
-
 export default function RootLayout({
   children,
 }: Readonly<{
