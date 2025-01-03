53130a39e05b46e054d5a6156e2df57c75628d96
beom <gb3187@naver.com>
2025-01-03 11:20:33 +0900

    Feat: 팔로우 등록/취소 낙관적업데이트 적용

--- src/components/UserProfile/UserProfileContent/index.tsx
+++ src/components/UserProfile/UserProfileContent/index.tsx
@@ -11,31 +11,33 @@ interface UserProfileContent {
 export const UserProfileContent = ({ userId }: UserProfileContent) => {
   const { completeResponses, isLoading } = useUserProfileQuery(Number(userId));
 
+  if (isLoading) {
+    return (
+      <div className="flex min-h-screen items-center justify-center">
+        <Spinner className="size-28" />
+      </div>
+    );
+  }
+
   return (
     <div className="mt-8 grid grid-cols-3 gap-8">
-      {isLoading ? (
-        <div className="col-span-3 flex items-center justify-center">
-          <Spinner />
-        </div>
-      ) : (
-        completeResponses.map((complete) =>
-          complete.completePic ? (
-            <Image
-              key={complete.completeId}
-              width={48}
-              height={48}
-              className="flex h-109 w-full rounded-4"
-              priority
-              src={complete.completePic}
-              alt="인증한 이미지"
-            />
-          ) : (
-            <div
-              key={complete.completeId}
-              className="flex h-109 w-full rounded-4 bg-gray-200"
-            />
-          ),
-        )
+      {completeResponses.map((complete) =>
+        complete.completePic ? (
+          <Image
+            key={complete.completeId}
+            width={48}
+            height={48}
+            className="flex h-109 w-full rounded-4"
+            priority
+            src={complete.completePic}
+            alt="인증한 이미지"
+          />
+        ) : (
+          <div
+            key={complete.completeId}
+            className="flex h-109 w-full rounded-4 bg-gray-200"
+          />
+        ),
       )}
     </div>
   );
