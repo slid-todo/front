d65210c2e90068834f312628d5826ac318d4db39
dudwns0213 <dudwns0213@gmail.com>
2025-01-04 16:36:58 +0900

    Feat: create CompletePost

---
+++ src/components/Complete/Post/index.tsx
@@ -0,0 +1,67 @@
+'use client';
+
+import { useRouter } from 'next/navigation';
+import { FollowsSkeleton } from '@/components/Skeletons/FollowsSkeleton';
+import { useGetCompleteDetailQuery } from '@/hooks/apis/Complete/useGetCompleteDetailQuery';
+import { PostContent } from '@/components/Follows/Post/PostContent';
+import { PostImage } from '@/components/Follows/Post/PostImage';
+import { PostLike } from '@/components/Follows/Post/PostLike';
+import { PostProfile } from '@/components/Follows/Post/PostProfile';
+import { PostGoalAndTodo } from './PostGoalAndTodo';
+
+interface CompletePostProps {
+  completeId: number;
+}
+
+export const CompletePost = (props: CompletePostProps) => {
+  const { completeId } = props;
+
+  const router = useRouter();
+
+  const { complete, isLoading, isError } =
+    useGetCompleteDetailQuery(completeId);
+
+  if (isError) {
+    router.push('/500');
+
+    return null;
+  }
+
+  return (
+    <div>
+      {isLoading ? (
+        <FollowsSkeleton />
+      ) : complete ? (
+        <div className="w-screen flex-col">
+          <div className="mr-16 flex justify-between">
+            <PostProfile
+              createdAt={complete.createdAt}
+              username="홍길동"
+              profilePic="https://slid-todo.s3.ap-northeast-2.amazonaws.com/auth/default_profilepic_mouse.png"
+              userId="2"
+            />
+            <PostLike
+              likeStatus={complete.likeStatus}
+              likeCount={complete.likeCount}
+              completeId={complete.completeId}
+            />
+          </div>
+
+          <PostImage
+            completePic={complete.completePic}
+            completeId={complete.completeId}
+          />
+
+          <PostContent text={complete.note} completeId={complete.completeId} />
+
+          <div className="flex justify-between gap-8 px-16 pt-8">
+            <PostGoalAndTodo>{complete.goalName}</PostGoalAndTodo>
+            <PostGoalAndTodo>{complete.todoName}</PostGoalAndTodo>
+          </div>
+        </div>
+      ) : (
+        <div>게시글이 없습니다.</div>
+      )}
+    </div>
+  );
+};
