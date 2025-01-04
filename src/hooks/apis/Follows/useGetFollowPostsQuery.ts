85bbe2590a895022f1f2eabf2b3cc58e4c78576a
dudwns0213 <dudwns0213@gmail.com>
2025-01-04 14:19:01 +0900

    Feat: useGetFollowPosts InfiniteQuery 적용

--- src/hooks/apis/Follows/useGetFollowPostsQuery.ts
+++ src/hooks/apis/Follows/useGetFollowPostsQuery.ts
@@ -1,27 +1,36 @@
-// hooks/useGetFollows.ts
-import { useQuery, UseQueryOptions } from '@tanstack/react-query';
+import {
+  useInfiniteQuery,
+  UseInfiniteQueryOptions,
+} from '@tanstack/react-query';
 import { AxiosError } from 'axios';
 
 import { GetFollowsResponse } from '@/types/Follows';
 import { GET } from '@/apis/services/httpMethod';
 import { API_ENDPOINTS } from '@/constants/ApiEndpoints';
 import { QUERY_KEYS } from '@/constants/QueryKeys';
+import { BaseInfiniteQueryResponse } from '@/types/response';
 
-export const getFollowPostsOptions = (): UseQueryOptions<
+export const getFollowPostsOptions = (): UseInfiniteQueryOptions<
   GetFollowsResponse,
-  AxiosError
+  AxiosError,
+  BaseInfiniteQueryResponse<GetFollowsResponse[]>
 > => ({
   queryKey: [QUERY_KEYS.FOLLOWS],
   queryFn: ({ pageParam = 0 }) =>
     GET<GetFollowsResponse>(
       `${API_ENDPOINTS.FOLLOW.GET}?lastCompleteId=${pageParam}&size=10`,
     ),
+  getNextPageParam: (lastPage) => {
+    const nextCursor = lastPage.data.nextCursor;
+    return nextCursor !== 0 ? nextCursor : undefined;
+  },
+  initialPageParam: 0,
 });
 
 export const useGetFollowPosts = () => {
-  const { data, ...etc } = useQuery(getFollowPostsOptions());
+  const { data, ...etc } = useInfiniteQuery(getFollowPostsOptions());
 
-  const follows = data?.data.content ?? [];
+  const follows = data?.pages.flatMap((page) => page.data.content) ?? [];
 
   return { follows, ...etc };
 };
