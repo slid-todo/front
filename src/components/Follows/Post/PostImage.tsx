b6cd9b0c68ab48ef6207e9d6b94e4f6df4d57a82
dudwns0213 <dudwns0213@gmail.com>
2025-01-04 15:05:34 +0900

    Feat: Image Click 시 complete 페이지로 이동

--- src/components/Follows/Post/PostImage.tsx
+++ src/components/Follows/Post/PostImage.tsx
@@ -1,18 +1,33 @@
 import Image from 'next/image';
+import { useRouter } from 'next/navigation';
 
 interface PostImageProps {
   completePic: string;
+  completeId: number;
 }
 
 export function PostImage(props: PostImageProps) {
-  const { completePic } = props;
+  const { completePic, completeId } = props;
+  const router = useRouter();
+
+  const handleClickImage = () => {
+    router.push(`/completes/${completeId}`);
+  };
 
   if (!completePic) {
-    return <div className="my-8 aspect-square w-full bg-slate-600" />;
+    return (
+      <div
+        className="my-8 aspect-square w-full cursor-pointer bg-slate-600"
+        onClick={handleClickImage}
+      />
+    );
   }
 
   return (
-    <div className="my-8 aspect-square w-full">
+    <div
+      className="my-8 aspect-square w-full cursor-pointer"
+      onClick={handleClickImage}
+    >
       <Image src={completePic} alt="post-image" fill />
     </div>
   );
