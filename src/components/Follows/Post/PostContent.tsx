8b51e26efa1945398b47d3716a5e0cd46daff6a1
dudwns0213 <dudwns0213@gmail.com>
2025-01-04 15:07:59 +0900

    Feat: Content 클릭 시 complete 이동 및 css 수정 (text 없을 경우)

--- src/components/Follows/Post/PostContent.tsx
+++ src/components/Follows/Post/PostContent.tsx
@@ -1,11 +1,29 @@
+import { useRouter } from 'next/navigation';
+import { cn } from '@/utils/className';
+
 interface PostContentProps {
-  text: string;
+  text: string | null;
+  completeId: number;
 }
 
 export function PostContent(props: PostContentProps) {
-  const { text } = props;
+  const { text, completeId } = props;
+
+  const router = useRouter();
+
+  const handleClickContent = () => {
+    router.push(`/completes/${completeId}`);
+  };
 
   return (
-    <div className="mx-16 my-8 text-sm-normal text-custom-gray-300">{text}</div>
+    <div
+      className={cn(
+        'mx-16 my-8 text-sm-normal cursor-pointer',
+        text ? 'text-custom-gray-300' : 'text-custom-gray-100',
+      )}
+      onClick={handleClickContent}
+    >
+      {text ?? '본문 내용이 없습니다.'}
+    </div>
   );
 }
