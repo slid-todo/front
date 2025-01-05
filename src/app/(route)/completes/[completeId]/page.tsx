fa55e4e7886e6b0fabbd9f31be5ec349475a7577
ejinn1 <ejinn1@naver.com>
2025-01-05 18:53:36 +0900

    Refactor: change complete page layout

--- src/app/(route)/completes/[completeId]/page.tsx
+++ src/app/(route)/completes/[completeId]/page.tsx
@@ -1,3 +1,4 @@
+import { PageContainer } from '@/components/common/PageContainer';
 import { CompleteComments } from '@/components/Complete/Comments';
 import { CompleteHeader } from '@/components/Complete/CompleteHeader';
 import { CompletePost } from '@/components/Complete/Post';
@@ -12,15 +13,15 @@ export default async function CompletePage({
   params: Promise<CompletePageProps>;
 }) {
   const { completeId } = await params;
-  return (
-    <div className="flex min-h-screen w-screen flex-col gap-16 bg-custom-white-100 py-16 md:px-200 xl:px-400 2xl:px-650">
-      <CompleteHeader />
 
+  return (
+    <PageContainer className="!px-0">
+      <CompleteHeader />
       <CompletePost completeId={completeId} />
 
       <div className="h-6 bg-custom-white-200" />
 
       <CompleteComments completeId={completeId} />
-    </div>
+    </PageContainer>
   );
 }
