aac680a83466c37e4b79106645900e27a4146043
beom <gb3187@naver.com>
2025-01-06 00:39:27 +0900

    Feat: 메인 페이지 임시 디자인

--- src/app/(route)/page.tsx
+++ src/app/(route)/page.tsx
@@ -1,5 +1,25 @@
 'use client';
 
+import { useRouter } from 'next/navigation';
+import { Logo } from '@/components/AuthPage/Logo';
+import { Button } from '@/components/common/Button/Button';
+import { PageContainer } from '@/components/common/PageContainer';
+
 export default function Home() {
-  return <div className="px-16 pt-48 sm:pt-0">asdf</div>;
+  const router = useRouter();
+
+  const handleClick = () => {
+    router.push('/dashboard');
+  };
+
+  return (
+    <PageContainer>
+      <div className="mb-48 flex flex-col items-center">
+        <Logo />
+      </div>
+      <Button size="large" onClick={handleClick}>
+        대시보드로 가기
+      </Button>
+    </PageContainer>
+  );
 }
