42d2616481d168bba4a19ae115ddd867c30ca214
ejinn1 <ejinn1@naver.com>
2025-01-05 18:45:15 +0900

    Feat: apply infinite scroll in dashboard follows and skeleton

--- src/components/Dashboard/Follower/FollowerStory/index.tsx
+++ src/components/Dashboard/Follower/FollowerStory/index.tsx
@@ -1,11 +1,11 @@
 import { FaHeart } from 'react-icons/fa6';
 
-import { FollowerTypes } from '@/mocks/followerData';
+import { ContentTypes } from '@/types/data';
 import { cn } from '@/utils/className';
 import { formatDateToRelativeTime } from '@/utils/date';
 
 interface FollowerStoryProps {
-  follower: FollowerTypes;
+  follower: ContentTypes;
 }
 
 export const FollowerStory = ({ follower }: FollowerStoryProps) => {
@@ -19,8 +19,8 @@ export const FollowerStory = ({ follower }: FollowerStoryProps) => {
       <div className="relative size-120 rounded-20 bg-custom-white-300">
         <FaHeart className={heartClass} />
       </div>
-      <div className="pt-8">
-        <span className="mr-4 text-xs-medium">{follower.name}</span>
+      <div className="ml-4 pt-8">
+        <span className="mr-4 text-xs-medium">{follower.username}</span>
         <span className="text-xs-normal text-custom-gray-200">
           {formatDateToRelativeTime(follower.createdAt)}
         </span>
--- src/components/Dashboard/Follower/index.tsx
+++ src/components/Dashboard/Follower/index.tsx
@@ -1,15 +1,27 @@
+'use client';
+
 import { Card } from '@/components/common/Card';
+import { Spinner } from '@/components/common/Spinner';
 import { DashboardItemContainer } from '@/components/Dashboard/DashboardItemContainer';
-import { FOLLOWER_DATA } from '@/mocks/followerData';
+import { FollowStorySkeleton } from '@/components/Skeletons/FollowStorySkeleton';
+import { useGetFollowPosts } from '@/hooks/apis/Follows/useGetFollowPostsQuery';
+import { useInfiniteScroll } from '@/hooks/useInfiniteScroll';
 import { FollowerStory } from './FollowerStory';
 
 export const Follwer = () => {
+  const { follows, isLoading, fetchNextPage, isFetchingNextPage } =
+    useGetFollowPosts();
+  const { observerRef } = useInfiniteScroll({ fetchNextPage, isLoading });
+
+  console.log(follows);
   return (
     <DashboardItemContainer
       title="팔로워 현황"
       className="relative mb-22 mt-16"
     >
-      {FOLLOWER_DATA.length === 0 ? (
+      {isLoading ? (
+        <FollowStorySkeleton />
+      ) : follows.length === 0 ? (
         <Card>
           <p className="text-sm-normal text-custom-gray-100">
             찍찍이들 팔로우 하고 인증 보기
@@ -17,9 +29,15 @@ export const Follwer = () => {
         </Card>
       ) : (
         <div className="flex snap-x snap-mandatory items-center gap-8  overflow-x-scroll scrollbar-hide">
-          {FOLLOWER_DATA.map((follower) => (
-            <FollowerStory key={follower.id} follower={follower} />
+          {follows.map((follower) => (
+            <FollowerStory key={follower.completeId} follower={follower} />
           ))}
+          {isFetchingNextPage && (
+            <span className="flex w-full justify-center">
+              <Spinner className="size-18" />
+            </span>
+          )}
+          <div ref={observerRef} style={{ width: '1px' }} />
         </div>
       )}
       <div className="absolute -bottom-22 -left-16 h-6 w-[calc(100%+32px)] bg-custom-white-200" />
