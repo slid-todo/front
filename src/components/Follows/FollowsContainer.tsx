444294f950490a596b91b4666fd579544a9d4758
ejinn1 <ejinn1@naver.com>
2025-01-05 17:49:52 +0900

    Refactor: apply follow page page container

--- src/components/Follows/FollowsContainer.tsx
+++ src/components/Follows/FollowsContainer.tsx
@@ -4,6 +4,7 @@ import { useRouter } from 'next/navigation';
 import { useGetFollowPosts } from '@/hooks/apis/Follows/useGetFollowPostsQuery';
 import { useInfiniteScroll } from '@/hooks/useInfiniteScroll';
 import { FollowsSkeleton } from '../Skeletons/FollowsSkeleton';
+import { PageContainer } from '../common/PageContainer';
 import { Spinner } from '../common/Spinner';
 import { Post } from './Post';
 
@@ -22,28 +23,30 @@ export const FollowsContainer = () => {
   }
 
   return (
-    <div className="mt-48">
-      <div className="fixed h-44 w-full bg-white px-16 py-8 text-xl-semibold text-custom-gray-300">
-        팔로워
+    <PageContainer className="!px-0">
+      <div>
+        <h1 className="fixed h-44 px-16 py-8 text-xl-semibold text-custom-gray-300">
+          팔로워
+        </h1>
+        <div className="mb-8 h-48" />
+        {isLoading ? (
+          <FollowsSkeleton />
+        ) : follows.length > 0 ? (
+          <div>
+            {follows.map((post) => (
+              <Post key={post.completeId} post={post} />
+            ))}
+            {isFetchingNextPage && (
+              <span className="flex w-full justify-center">
+                <Spinner className="size-18" />
+              </span>
+            )}
+            <div ref={observerRef} style={{ height: '1px' }} />
+          </div>
+        ) : (
+          <div>게시글이 없습니다.</div>
+        )}
       </div>
-      <div className="mb-8 h-48" />
-      {isLoading ? (
-        <FollowsSkeleton />
-      ) : follows.length > 0 ? (
-        <div>
-          {follows.map((post) => (
-            <Post key={post.completeId} post={post} />
-          ))}
-          {isFetchingNextPage && (
-            <span className="flex w-full justify-center">
-              <Spinner className="size-18" />
-            </span>
-          )}
-          <div ref={observerRef} style={{ height: '1px' }} />
-        </div>
-      ) : (
-        <div>게시글이 없습니다.</div>
-      )}
-    </div>
+    </PageContainer>
   );
 };
