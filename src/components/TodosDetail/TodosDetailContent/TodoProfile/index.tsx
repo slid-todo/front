900d04485c3f068b5ec631cee108e4ceba9fd8ba
beom <gb3187@naver.com>
2025-01-05 18:50:00 +0900

    Refactor: 인증이 필요없는 할일 목록 컴포넌트 생성 및 적용

--- src/components/TodosDetail/TodosDetailContent/TodoProfile/index.tsx
+++ src/components/TodosDetail/TodosDetailContent/TodoProfile/index.tsx
@@ -1,6 +1,6 @@
-import { FaCamera } from 'react-icons/fa6';
+import { BasicTodoItem } from './BasicTodoItem';
 
-interface TodoProfileProps {
+export interface TodoProfileProps {
   goalColor: string | undefined;
   goalTitle: string | undefined;
   todoTitle: string | undefined;
@@ -12,17 +12,10 @@ export const TodoProfile = ({
   todoTitle,
 }: TodoProfileProps) => {
   return (
-    <div className="flex items-center gap-16">
-      <div
-        className="flex-center my-8 size-56 cursor-pointer rounded-16"
-        style={{ backgroundColor: goalColor }}
-      >
-        <FaCamera fill="white" />
-      </div>
-      <div className="flex flex-col items-start">
-        <span className="text-xs-medium text-custom-gray-100">{goalTitle}</span>
-        <span className="text-base-medium">{todoTitle}</span>
-      </div>
-    </div>
+    <BasicTodoItem
+      goalColor={goalColor}
+      goalTitle={goalTitle}
+      todoTitle={todoTitle}
+    />
   );
 };
