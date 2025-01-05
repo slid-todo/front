f8796e41946a56291ad70f60c70d368bffd9d7cd
dudwns0213 <dudwns0213@gmail.com>
2025-01-05 15:17:15 +0900

    Feat: create CompleteComments

---
+++ src/components/Complete/Comments/index.tsx
@@ -0,0 +1,41 @@
+'use client';
+
+import { useRouter } from 'next/navigation';
+import { FollowsSkeleton } from '@/components/Skeletons/FollowsSkeleton';
+import { useGetCompleteDetailQuery } from '@/hooks/apis/Complete/useGetCompleteDetailQuery';
+import { CommentList } from './CommentList';
+import { CommentInput } from './CommentInput';
+
+interface CompleteCommentsProps {
+  completeId: number;
+}
+
+export const CompleteComments = ({ completeId }: CompleteCommentsProps) => {
+  const router = useRouter();
+  const { complete, isLoading, isError } =
+    useGetCompleteDetailQuery(completeId);
+
+  if (isError) {
+    router.push('/500');
+    return null;
+  }
+
+  return (
+    <div className="relative min-h-screen pb-20">
+      {isLoading ? (
+        <FollowsSkeleton />
+      ) : complete ? (
+        <div className="mx-auto w-full max-w-3xl flex-col px-4">
+          <span className="text-sm-medium text-custom-gray-100">
+            댓글 ({complete.commentCount})
+          </span>
+          <CommentList comments={complete.comments} />
+        </div>
+      ) : (
+        <div className="text-center text-gray-500">게시글이 없습니다.</div>
+      )}
+      {/* 고정된 댓글 입력 칸 */}
+      <CommentInput completeId={completeId} />
+    </div>
+  );
+};
