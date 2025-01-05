58b30c207c394272cdc89c7dade2f83c5f069e36
ejinn1 <ejinn1@naver.com>
2025-01-05 21:57:51 +0900

    Feat: add follow story image

--- src/components/Dashboard/Follower/FollowerStory/index.tsx
+++ src/components/Dashboard/Follower/FollowerStory/index.tsx
@@ -1,5 +1,6 @@
 import { FaHeart } from 'react-icons/fa6';
 
+import Image from 'next/image';
 import { useRouter } from 'next/navigation';
 
 import { ContentTypes } from '@/types/data';
@@ -12,10 +13,11 @@ interface FollowerStoryProps {
 
 export const FollowerStory = ({ follower }: FollowerStoryProps) => {
   const router = useRouter();
+  const pic = follower.completePic;
 
   const heartClass = cn(
     'absolute right-8 top-8 size-28 p-4',
-    follower.likeStatus ? 'text-error' : 'text-custom-gray-200',
+    follower.likeStatus ? 'text-error' : 'text-custom-white-300',
   );
 
   const handleClickImage = () => {
@@ -27,9 +29,21 @@ export const FollowerStory = ({ follower }: FollowerStoryProps) => {
       onClick={handleClickImage}
       className="shrink-0 cursor-pointer snap-start"
     >
-      <div className="relative size-120 rounded-20 bg-custom-white-300">
+      <div className="relative">
+        {pic ? (
+          <Image
+            src={pic}
+            width={120}
+            height={120}
+            alt="팔로워 인증 사진"
+            className="size-120 rounded-20 object-cover"
+          />
+        ) : (
+          <div className="size-120 rounded-20 bg-custom-gray-100" />
+        )}
         <FaHeart className={heartClass} />
       </div>
+
       <div className="ml-4 pt-8">
         <span className="mr-4 text-xs-medium">{follower.username}</span>
         <span className="text-xs-normal text-custom-gray-200">
