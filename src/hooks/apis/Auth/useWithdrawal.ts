3411d2b194f1bb9c8eec46e5fe277e64d937e75d
beom <gb3187@naver.com>
2025-01-05 19:36:02 +0900

    Refactor: 회원탈퇴 로직 수정

--- src/hooks/apis/Auth/useWithdrawal.ts
+++ src/hooks/apis/Auth/useWithdrawal.ts
@@ -1,18 +1,21 @@
-import { useMutation } from '@tanstack/react-query';
+import { useMutation, useQueryClient } from '@tanstack/react-query';
+import { useRouter } from 'next/navigation';
 import { DELETE } from '@/apis/services/httpMethod';
 import { WithdrawalResponse } from '@/types/response';
 import { API_ENDPOINTS } from '@/constants/ApiEndpoints';
 import { notify } from '@/store/useToastStore';
-import { useLogout } from '@/hooks/useLogout';
 
 export const useWithdrawal = () => {
-  const { logout } = useLogout();
+  const queryClient = useQueryClient();
+  const router = useRouter();
 
   return useMutation({
     mutationFn: () => DELETE<WithdrawalResponse>(API_ENDPOINTS.AUTH.WITHDRAWAL),
     onSuccess: () => {
       notify('success', '회원탈퇴에 성공하였습니다.', 3000);
-      logout();
+      document.cookie = 'token=; max-age=0; path=/;';
+      queryClient.clear();
+      router.push('/signin');
     },
     onError: (error) => {
       notify('error', '회원탈퇴에 실패하였습니다.', 3000);
