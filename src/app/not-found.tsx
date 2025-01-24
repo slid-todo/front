3168da249f8cb1ec36d5e6aa838175656b61ddde
beom <gb3187@naver.com>
2025-01-24 22:13:14 +0900

    Feat: 404 페이지 제작

---
+++ src/app/not-found.tsx
@@ -0,0 +1,42 @@
+'use client';
+
+import { useRouter } from 'next/navigation';
+import LogoMain from '@/assets/svg/svg-logo-main.svg';
+import { Button } from '@/components/common/Button/Button';
+import { Header } from '@/components/common/Header';
+import { PageContainer } from '@/components/common/PageContainer';
+import { Sidebar } from '@/components/Sidebar';
+
+export default function NotFound() {
+  const router = useRouter();
+
+  return (
+    <div className="flex">
+      <Sidebar />
+      <Header />
+      <PageContainer header={true} className="flex-center">
+        <div className="flex-center flex-col gap-15">
+          <LogoMain className="w-90 animate-bounce" />
+          <h1 className="text-3xl-bold text-custom-gray-300">
+            페이지를 찾을 수 없어요.
+          </h1>
+          <p className="text-lg-medium text-custom-gray-100">
+            뒤로 가거나 홈으로 이동해 보세요.
+          </p>
+          <div className="flex gap-8">
+            <Button
+              primary={true}
+              size="small"
+              onClick={() => router.push('/dashboard')}
+            >
+              홈으로 돌아가기
+            </Button>
+            <Button primary={false} size="small" onClick={() => router.back()}>
+              이전 페이지
+            </Button>
+          </div>
+        </div>
+      </PageContainer>
+    </div>
+  );
+}
