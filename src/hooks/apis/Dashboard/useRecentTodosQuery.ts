a77a42004575eb5c5cfaa39dc78755c8f42f02a3
ejinn1 <ejinn1@naver.com>
2025-03-05 00:47:18 +0900

    Feat: add suspense for improved loading state handling

--- src/hooks/apis/Dashboard/useRecentTodosQuery.ts
+++ src/hooks/apis/Dashboard/useRecentTodosQuery.ts
@@ -1,24 +1,30 @@
-import { useQuery, UseQueryOptions } from '@tanstack/react-query';
+import {
+  useSuspenseQuery,
+  UseSuspenseQueryOptions,
+} from '@tanstack/react-query';
 import { AxiosError } from 'axios';
 
 import { GET } from '@/apis/services/httpMethod';
 import { API_ENDPOINTS } from '@/constants/ApiEndpoints';
 import { QUERY_KEYS } from '@/constants/QueryKeys';
+import { getQueryClient } from '@/lib/query/getQueryClient';
 import { RecentTodosResponse } from '@/types/Dashboard';
 
-export const recentTodosOptions = (): UseQueryOptions<
-  RecentTodosResponse,
-  AxiosError
-> => ({
+export const recentTodosOptions = (
+  token?: string,
+): UseSuspenseQueryOptions<RecentTodosResponse, AxiosError> => ({
   queryKey: [QUERY_KEYS.RECENT_TODOS],
   queryFn: () =>
     GET<RecentTodosResponse>(
       `${API_ENDPOINTS.TODOS.GET_ALL}?lastTodoId=0&size=3`,
+      token,
     ),
+  initialData: () =>
+    getQueryClient().getQueryData([QUERY_KEYS.RECENT_TODOS]) || undefined,
 });
 
 export const useRecentTodosQuery = () => {
-  const { data, ...etc } = useQuery(recentTodosOptions());
+  const { data, ...etc } = useSuspenseQuery(recentTodosOptions());
   const todos = data?.data.content ?? [];
 
   return { todos, ...etc };
--- src/hooks/apis/Dashboard/useTodayProgressQuery.ts
+++ src/hooks/apis/Dashboard/useTodayProgressQuery.ts
@@ -1,22 +1,27 @@
-import { useQuery, UseQueryOptions } from '@tanstack/react-query';
+import {
+  useSuspenseQuery,
+  UseSuspenseQueryOptions,
+} from '@tanstack/react-query';
 import { AxiosError } from 'axios';
 
 import { GET } from '@/apis/services/httpMethod';
 import { API_ENDPOINTS } from '@/constants/ApiEndpoints';
 import { QUERY_KEYS } from '@/constants/QueryKeys';
+import { getQueryClient } from '@/lib/query/getQueryClient';
 import { TodayProgressResponse } from '@/types/Dashboard';
 
-export const todayProgressOptions = (): UseQueryOptions<
-  TodayProgressResponse,
-  AxiosError
-> => ({
+export const todayProgressOptions = (
+  token?: string,
+): UseSuspenseQueryOptions<TodayProgressResponse, AxiosError> => ({
   queryKey: [QUERY_KEYS.TODAY_PROGRESS],
   queryFn: () =>
-    GET<TodayProgressResponse>(API_ENDPOINTS.TODOS.GET_TODAY_PROGRESS),
+    GET<TodayProgressResponse>(API_ENDPOINTS.TODOS.GET_TODAY_PROGRESS, token),
+  initialData: () =>
+    getQueryClient().getQueryData([QUERY_KEYS.TODAY_PROGRESS]) || undefined,
 });
 
 export const useTodayProgressQuery = () => {
-  const { data, ...etc } = useQuery(todayProgressOptions());
+  const { data, ...etc } = useSuspenseQuery(todayProgressOptions());
   const progress = data?.data.progress ?? 0;
 
   return { progress, ...etc };
