1101a3e0a56b8e0aa8b74b8408bbf8c31f256073
ejinn1 <ejinn1@naver.com>
2025-03-06 16:08:43 +0900

    Refactor: remove suspense from myProgress and use useQuery

--- src/hooks/apis/Dashboard/useTodayProgressQuery.ts
+++ src/hooks/apis/Dashboard/useTodayProgressQuery.ts
@@ -1,27 +1,21 @@
-import {
-  useSuspenseQuery,
-  UseSuspenseQueryOptions,
-} from '@tanstack/react-query';
+import { useQuery, UseQueryOptions } from '@tanstack/react-query';
 import { AxiosError } from 'axios';
 
 import { GET } from '@/apis/services/httpMethod';
 import { API_ENDPOINTS } from '@/constants/ApiEndpoints';
 import { QUERY_KEYS } from '@/constants/QueryKeys';
-import { getQueryClient } from '@/lib/query/getQueryClient';
 import { TodayProgressResponse } from '@/types/Dashboard';
 
 export const todayProgressOptions = (
   token?: string,
-): UseSuspenseQueryOptions<TodayProgressResponse, AxiosError> => ({
+): UseQueryOptions<TodayProgressResponse, AxiosError> => ({
   queryKey: [QUERY_KEYS.TODAY_PROGRESS],
   queryFn: () =>
     GET<TodayProgressResponse>(API_ENDPOINTS.TODOS.GET_TODAY_PROGRESS, token),
-  initialData: () =>
-    getQueryClient().getQueryData([QUERY_KEYS.TODAY_PROGRESS]) || undefined,
 });
 
 export const useTodayProgressQuery = () => {
-  const { data, ...etc } = useSuspenseQuery(todayProgressOptions());
+  const { data, ...etc } = useQuery(todayProgressOptions());
   const progress = data?.data.progress ?? 0;
 
   return { progress, ...etc };
