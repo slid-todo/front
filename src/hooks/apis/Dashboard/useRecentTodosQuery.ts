737d3e26ba0f1dd2f52f4214fbacc67f34a5e88b
ejinn1 <ejinn1@naver.com>
2025-03-08 16:43:10 +0900

    Refactor: remove initialData in recent todos and goals

--- src/hooks/apis/Dashboard/useRecentTodosQuery.ts
+++ src/hooks/apis/Dashboard/useRecentTodosQuery.ts
@@ -7,7 +7,6 @@ import { AxiosError } from 'axios';
 import { GET } from '@/apis/services/httpMethod';
 import { API_ENDPOINTS } from '@/constants/ApiEndpoints';
 import { QUERY_KEYS } from '@/constants/QueryKeys';
-import { getQueryClient } from '@/lib/query/getQueryClient';
 import { RecentTodosResponse } from '@/types/Dashboard';
 
 export const recentTodosOptions = (
@@ -19,8 +18,6 @@ export const recentTodosOptions = (
       `${API_ENDPOINTS.TODOS.GET_ALL}?lastTodoId=0&size=3`,
       token,
     ),
-  initialData: () =>
-    getQueryClient().getQueryData([QUERY_KEYS.RECENT_TODOS]) || undefined,
 });
 
 export const useRecentTodosQuery = () => {
